7d10195f60b50a942964d80951f8d22a2067c320
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-08 13:12:42 -0400

    wip issue2 form data binding

--- client/src/app/components/new-solo-game-form/new-solo-game-form.component.ts
+++ client/src/app/components/new-solo-game-form/new-solo-game-form.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SoloGameSettings } from './solo-game-settings.interface';
 
 @Component({
     selector: 'app-new-solo-game-form',
     templateUrl: './new-solo-game-form.component.html',
     styleUrls: ['./new-solo-game-form.component.scss'],
 })
-export class NewSoloGameFormComponent {}
+export class NewSoloGameFormComponent implements OnInit {
+    soloGameSettings: SoloGameSettings = {
+        playerName: undefined,
+        adversaryLevel: undefined,
+        timePerTurn: null,
+    };
+
+    ngOnInit() {
+        console.log(this.soloGameSettings);
+    }
+
+    playGame(): void {
+        console.log(this.soloGameSettings);
+    }
+}
---
+++ client/src/app/components/new-solo-game-form/solo-game-settings.interface.ts
@@ -0,0 +1,5 @@
+export interface SoloGameSettings {
+    playerName: string | undefined;
+    adversaryLevel: string | undefined;
+    timePerTurn: number | null;
+}
