29bee30160df185f8092facd7d1e45b37efe57d7
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-08 11:04:52 -0400

    wip ui issue 2 new game form

---
+++ client/src/app/components/new-solo-game-form/new-solo-game-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-new-solo-game-form',
+    templateUrl: './new-solo-game-form.component.html',
+    styleUrls: ['./new-solo-game-form.component.scss'],
+})
+export class NewSoloGameFormComponent {}
--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -8,20 +8,20 @@ import { MatCardModule } from '@angular/material/card';
 // import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
-// import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 // import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-// import { MatInputModule } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 // import { MatListModule } from '@angular/material/list';
 // import { MatMenuModule } from '@angular/material/menu';
 // import { MatPaginatorModule } from '@angular/material/paginator';
 // import { MatProgressBarModule } from '@angular/material/progress-bar';
 // import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-// import { MatSelectModule } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 // import { MatSidenavModule } from '@angular/material/sidenav';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-// import { MatSliderModule } from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
 // import { MatSnackBarModule } from '@angular/material/snack-bar';
 // import { MatSortModule } from '@angular/material/sort';
 // import { MatStepperModule } from '@angular/material/stepper';
@@ -41,19 +41,19 @@ const modules = [
     // MatDatepickerModule,
     MatDialogModule,
     MatExpansionModule,
-    // MatFormFieldModule,
+    MatFormFieldModule,
     // MatGridListModule,
     MatIconModule,
-    // MatInputModule,
+    MatInputModule,
     // MatListModule,
     // MatMenuModule,
     // MatPaginatorModule,
     // MatProgressBarModule,
     // MatProgressSpinnerModule,
     MatRadioModule,
-    // MatSelectModule,
+    MatSelectModule,
     // MatSidenavModule,
-    // MatSliderModule,
+    MatSliderModule,
     // MatSlideToggleModule,
     // MatSnackBarModule,
     // MatSortModule,
