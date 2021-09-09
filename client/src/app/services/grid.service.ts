83977518fec4fced700319a5bad4df493c6b06e1
Samuel <laporte.samuel2000@gmail.com>
2021-09-08 22:59:43 -0400

    WIP grid board

--- client/src/app/services/grid.service.ts
+++ client/src/app/services/grid.service.ts
@@ -11,6 +11,12 @@ export const NUMBER_GRID_CELL = 15;
 })
 export class GridService {
     gridContext: CanvasRenderingContext2D;
+
+    containerHeight: number;
+    containerWidth: number;
+
+    maxGridSize: number;
+
     private canvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
 
     // TODO : pas de valeurs magiques!! Faudrait avoir une meilleure manière de le faire
@@ -36,17 +42,18 @@ export class GridService {
         this.gridContext.stroke();
     }
 
-    drawBoard(witdh: number, height: number) {
-        const step = witdh / NUMBER_GRID_CELL;
+    drawBoard() {
+        this.calculateMinMax();
+        const step = this.maxGridSize / NUMBER_GRID_CELL;
         // size of canvas
-        for (let x = 0; x <= witdh; x += step) {
+        for (let x = 0; x <= this.maxGridSize; x += step) {
             this.gridContext.moveTo(0.5 + x, 0);
-            this.gridContext.lineTo(0.5 + x, height);
+            this.gridContext.lineTo(0.5 + x, this.maxGridSize);
         }
 
-        for (let x = 0; x <= height; x += step) {
+        for (let x = 0; x <= this.maxGridSize; x += step) {
             this.gridContext.moveTo(0, 0.5 + x);
-            this.gridContext.lineTo(witdh, 0.5 + x);
+            this.gridContext.lineTo(this.maxGridSize, 0.5 + x);
         }
 
         this.gridContext.strokeStyle = 'black';
@@ -69,5 +76,9 @@ export class GridService {
     // Remove later
     get height(): number {
         return this.canvasSize.y;
+    }
+
+    calculateMinMax(): void {
+        this.maxGridSize = this.containerHeight < this.containerWidth ? this.containerHeight : this.containerWidth;
     }
 }
