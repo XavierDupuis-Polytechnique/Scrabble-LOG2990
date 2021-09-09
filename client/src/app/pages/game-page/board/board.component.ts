d80882cd55d6423ca139584d7f612b09622e9b9e
Samuel <laporte.samuel2000@gmail.com>
2021-09-08 22:59:43 -0400

    WIP grid board

--- client/src/app/pages/game-page/board/board.component.ts
+++ client/src/app/pages/game-page/board/board.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GridService } from '@app/services/grid.service';
 
 export const BOARD_WIDTH = 500;
@@ -9,21 +9,27 @@ export const BOARD_HEIGTH = 500;
     templateUrl: './board.component.html',
     styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements AfterViewInit {
+export class BoardComponent implements AfterViewInit, OnInit {
     @ViewChild('gridCanvas', { static: false }) private gridCanvas!: ElementRef<HTMLCanvasElement>;
-
     constructor(private readonly gridService: GridService) {}
 
+    ngOnInit(): void {
+        const gridElement = document.getElementById('gridContainer');
+        if (gridElement?.clientHeight !== undefined && gridElement.clientWidth !== undefined) {
+            this.gridService.containerHeight = gridElement.clientHeight;
+            this.gridService.containerWidth = gridElement.clientWidth;
+        }
+    }
     ngAfterViewInit(): void {
         this.gridService.gridContext = this.gridCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        this.gridService.drawBoard(this.width, this.height);
+        this.gridService.drawBoard();
     }
 
     get width(): number {
-        return BOARD_WIDTH;
+        return this.gridService.containerWidth;
     }
 
     get height(): number {
-        return BOARD_HEIGTH;
+        return this.gridService.containerHeight;
     }
 }
