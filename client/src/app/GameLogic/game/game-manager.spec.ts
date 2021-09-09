a4f0a44ac1181e1de0e99123e25266460d0e3565
Darkforce689 <60223836+Darkforce689@users.noreply.github.com>
2021-09-09 17:37:38 -0400

    WIP

---
+++ client/src/app/GameLogic/game/game-manager.spec.ts
@@ -0,0 +1,7 @@
+import { GameManager } from './game-manager';
+
+describe('GameManager', () => {
+  it('should create an instance', () => {
+    expect(new GameManager()).toBeTruthy();
+  });
+});
--- client/src/app/GameLogic/player/bot.ts
+++ client/src/app/GameLogic/player/bot.ts
@@ -1,2 +1,11 @@
-export class Bot {
+import { Player } from './player';
+
+export class Bot extends Player {
+    constructor(name: string) {
+        super(name);
+    }
+
+    hello(): void {
+        console.log('hello from bot ' + this.name);
+    }
 }
