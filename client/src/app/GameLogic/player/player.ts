b1d57e01e6c73490a14695a954b37dffdc793597
Darkforce689 <60223836+Darkforce689@users.noreply.github.com>
2021-09-09 16:58:02 -0400

    WIP non fonctionnel gamelogic

---
+++ client/src/app/GameLogic/player/player.ts
@@ -0,0 +1,5 @@
+export class Player {
+    name: string;
+    isActive: boolean;
+    letterRack: string[];
+}
---
+++ client/src/app/GameLogic/player/user.ts
@@ -0,0 +1,3 @@
+import { Player } from './player';
+
+export class User extends Player {}
