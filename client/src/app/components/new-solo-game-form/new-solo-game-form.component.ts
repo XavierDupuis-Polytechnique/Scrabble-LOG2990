20f7b907a3866bdaa608483e51d1654199460f13
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-10 13:10:07 -0400

    wip new game form validation
    form validation and default value when form reset and time per turn slider implementation

--- client/src/app/components/new-solo-game-form/new-solo-game-form.component.ts
+++ client/src/app/components/new-solo-game-form/new-solo-game-form.component.ts
@@ -1,5 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const MAX_NAME_LENGTH = 50;
+const MIN_NAME_LENGTH = 4;
+
+const DEFAULT_TIME_PER_TURN = 60000;
+const MIN_TIME_PER_TURN = 30000;
+const MAX_TIME_PER_TURN = 300000;
+const STEP_TIME_PER_TURN = 30000;
 
 @Component({
     selector: 'app-new-solo-game-form',
@@ -9,19 +17,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class NewSoloGameFormComponent {
     @Output() cancelClick = new EventEmitter<void>();
     @Output() playClick = new EventEmitter<void>();
-
     soloGameSettingsForm = new FormGroup({
-        playerName: new FormControl(''),
-        adversaryDifficulty: new FormControl(''),
-        timePerTurn: new FormControl(''),
+        playerName: new FormControl('', [Validators.required, Validators.minLength(MIN_NAME_LENGTH), Validators.maxLength(MAX_NAME_LENGTH)]),
+        adversaryDifficulty: new FormControl('', [Validators.required]),
+        timePerTurn: new FormControl(DEFAULT_TIME_PER_TURN, [
+            Validators.required,
+            Validators.min(MIN_TIME_PER_TURN),
+            Validators.max(MAX_TIME_PER_TURN),
+        ]),
     });
+
+    minTimePerTurn = MIN_TIME_PER_TURN;
+    maxTimePerTurn = MAX_TIME_PER_TURN;
+    stepTimePerTurn = STEP_TIME_PER_TURN;
 
     playGame(): void {
         this.playClick.emit();
     }
 
     cancel(): void {
-        this.soloGameSettingsForm.reset();
+        this.soloGameSettingsForm.reset({
+            playerName: '',
+            adversaryDifficulty: '',
+            timePerTurn: DEFAULT_TIME_PER_TURN,
+        });
         this.cancelClick.next();
+    }
+
+    get formValid() {
+        return this.soloGameSettingsForm.valid;
     }
 }
