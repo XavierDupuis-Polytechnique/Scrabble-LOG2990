3f8843698644997ac43b3699beb0fa1ed0493230
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-13 15:29:06 -0400

    lint letter-bag

--- client/src/app/GameLogic/game/letter-bag.ts
+++ client/src/app/GameLogic/game/letter-bag.ts
@@ -1,47 +1,52 @@
 import { GameLetter } from './game-letter';
 
+const GAME_LETTERS = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    '*',
+];
+const PLAYER_LETTER_COUNT = 7;
+
 export class LetterBag {
-    static GameLettersLetters = [
-        'A',
-        'B',
-        'C',
-        'D',
-        'E',
-        'F',
-        'G',
-        'H',
-        'I',
-        'J',
-        'K',
-        'L',
-        'M',
-        'N',
-        'O',
-        'P',
-        'Q',
-        'R',
-        'S',
-        'T',
-        'U',
-        'V',
-        'W',
-        'X',
-        'Y',
-        'Z',
-        '*',
-    ];
+    static readonly gameLetters = GAME_LETTERS;
 
     // Lint est pas parfait : https://github.com/typescript-eslint/typescript-eslint/issues/945
-    static gameLettersCount = [9, 2, 2, 3, 15, 2, 2, 2, 8, 1, 1, 5, 3, 6, 6, 2, 1, 6, 6, 6, 6, 2, 1, 1, 1, 1, 2];
-    static gameLettersValue = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10, 0];
-    static playerLetterCount = 7;
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    static readonly gameLettersCount = [9, 2, 2, 3, 15, 2, 2, 2, 8, 1, 1, 5, 3, 6, 6, 2, 1, 6, 6, 6, 6, 2, 1, 1, 1, 1, 2];
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    static readonly gameLettersValue = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10, 0];
+    static readonly playerLetterCount = PLAYER_LETTER_COUNT;
 
     gameLetters: GameLetter[] = [];
 
     constructor() {
-        for (let letterIndex = 0; letterIndex < LetterBag.GameLettersLetters.length; letterIndex++) {
+        for (let letterIndex = 0; letterIndex < LetterBag.gameLetters.length; letterIndex++) {
             for (let count = 0; count < LetterBag.gameLettersCount[letterIndex]; count++) {
-                this.gameLetters.push(new GameLetter(LetterBag.GameLettersLetters[letterIndex], LetterBag.gameLettersValue[letterIndex]));
+                this.gameLetters.push(new GameLetter(LetterBag.gameLetters[letterIndex], LetterBag.gameLettersValue[letterIndex]));
             }
         }
         this.displayNumberGameLettersLeft();
