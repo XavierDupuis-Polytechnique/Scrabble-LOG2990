86eeeae7bf2d004229acdfccde81d4a2ba5c5ebc
dasadou <65833938+dasadou@users.noreply.github.com>
2021-09-14 14:44:10 -0400

    ajout de stringToLetter

--- client/src/app/pages/game-page/chat-box/chat-box.component.ts
+++ client/src/app/pages/game-page/chat-box/chat-box.component.ts
@@ -1,26 +1,21 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommandParserService } from './command-parser/command-parser.service';
+// import { MatInput } from '@angular/material/input';
 
-commandParser: CommandParserService;
 // let noInput:boolean = false;
 @Component({
     selector: 'app-chat-box',
     templateUrl: './chat-box.component.html',
     styleUrls: ['./chat-box.component.scss'],
 })
-/*
-enum messageForwarder {
-    SystemMessage,
-    PlayerMessage,
-    OpponentMesage
-}
 
-*/
 // Mettre un form pour desctiver le
 // boutton s'il n y a rien d'entree
 export class ChatBoxComponent {
     // Avoir une autre fonction linker/binder aver le placement etc...
+   // @ViewChild('userEntry') userEntry: MatInput;
     @ViewChild('userEntry') userEntry: ElementRef;
+
     @ViewChild('sharedChat') sharedChat: HTMLTextAreaElement;
     @ViewChild('Chat') chat: ElementRef;
 
@@ -29,25 +24,21 @@ export class ChatBoxComponent {
     }
 
     sendChat() {
+        console.log(this.userEntry.nativeElement.value);
         const userEntry: string = this.userEntry.nativeElement.value;
-        if (userEntry == '') {
-          //  noInput = true;
-            return;
-        }
-        console.log(userEntry);
+
+        if (userEntry == '') return;
+
         // Verifie s'i s'agit d'une commande
-        let verifier = this.commandParserService.isCommand(userEntry, document.createElement('p'));
+        let verifier = this.commandParserService.isCommand(userEntry, document.createElement('div'));
         let verifiedMessage = verifier[0];
-        let command = verifier[1];
+        //let command = verifier[1];
 
-        // var messageCount = this.chat.nativeElement.;
+        verifiedMessage.innerHTML += userEntry + '\n';// Peut-etre l'enlever
+        verifiedMessage.id = "message" /*+ messageCount*/;
+        //count how many childs before adding to have the ID
 
-        if (!command) {
-            verifiedMessage.innerHTML = userEntry + '\n';// Peut etre l'enlever
-            verifiedMessage.id = "message" /*+ messageCount*/;
-            //count how many childs before adding to have the ID
 
-        }
         // A ameliorer
         this.chat.nativeElement.appendChild(verifiedMessage);
         verifiedMessage.scrollIntoView(true);
--- client/src/app/pages/game-page/chat-box/command-parser/command-parser.service.ts
+++ client/src/app/pages/game-page/chat-box/command-parser/command-parser.service.ts
@@ -1,65 +1,96 @@
 import { Injectable } from '@angular/core';
-
+//import { ExchangeLetter } from '@app/GameLogic/actions/exchange-letter';
+//import { Letter } from 'src/app/GameLogic/game/letter.interface';
+//import { Player } from '@app/GameLogic/player/player';
+//import { LetterBag } from '@app/GameLogic/game/letter-bag';
 enum Command {
-  InvalidCommand,
-  Debug,
+  Debug = '!debug',
   Help = '!aide',
   Place = '!placer',
-  Exchanger = '!échanger',
+  Exchange = '!échanger',
   Pass = '!passer',
 }
+//Placeholder 
+//player: Player;
 
 @Injectable({
   providedIn: 'root'
 })
 
-
+// {char: "A", value:"1"}
 export class CommandParserService {
 
   constructor() { }
 
+  /* stringToLetter(letter: string) {
+     let lettersToExchange: Letter[] = [];
+     if (letter == null) return;
+     if (letter.length > 0) {
+       console.log(letter);/*
+       for (var _i = 0; _i < letter.length; _i++) {
+         lettersToExchange[_i].char = letter[_i];
+         lettersToExchange[_i].value = LetterBag.gameLettersValue[letter.charCodeAt(_i) - 72];
+ 
+       }
+       for (var _i = 0; _i < letter.length; _i++) {
+         console.log(lettersToExchange[_i].value);
+       }
+       return lettersToExchange;
+ 
+     }
+     return;
+   }*/
 
   isCommand(toVerify: string, Message: HTMLParagraphElement): [HTMLParagraphElement, boolean] {
     // Couper l'entry par espace pour verifier s'il s'agit d'une commande
+    console.log(toVerify.split(' ')[0]);
     const commandCondition = toVerify.split(' ')[0];
     if (commandCondition[0] == '!') {
+      Message.style.color = 'red';
 
       switch (commandCondition) {
+
         case Command.Place: {
           Message.classList.add('systemMessage');
-          Message.innerHTML = 'Commande';
+          Message.innerHTML = ' Commande ';
 
           return [Message, true];
         }
+        case Command.Exchange: {
+          // this.stringToLetter(toVerify.split(' ')[1]);
+          Message.classList.add('systemMessage');
+          return [Message, true];
+          // const comExchange: ExchangeLetter = new ExchangeLetter(player, this.stringToLetter(toVerify.split(' ')[1]));
+        }
+
+
         case Command.Pass: {
           Message.classList.add('systemMessage');
-          Message.innerHTML = 'Commande';
+          Message.innerHTML = 'Commande ';
 
           return [Message, true];
         }
         case Command.Debug: {
           Message.classList.add('systemMessage');
-          Message.innerHTML = 'Commande';
+          Message.innerHTML = 'Commande ';
 
           return [Message, true];
         }
         case Command.Help: {
           Message.classList.add('systemMessage');
-          Message.innerHTML = 'Commande';
-
-          return [Message, true];
-
-        }
-        case Command.InvalidCommand: {
-          Message.classList.add('systemMessage');
-          Message.innerHTML = 'Commande';
+          Message.innerHTML = 'Commande ';
 
           return [Message, true];
 
         }
         default: {
-          return [Message, false];
+          Message.classList.add('systemMessage');
+          Message.innerHTML = 'Commande invalide';
+
+          return [Message, true];
+
         }
+
       }
     }
     return [Message, false];
