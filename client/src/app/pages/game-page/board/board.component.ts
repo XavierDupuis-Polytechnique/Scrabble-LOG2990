8d14e4ebf1145d858edab38dda2ca48e1f8a5630
dasadou <65833938+dasadou@users.noreply.github.com>
2021-09-19 00:55:42 -0400

    bouton pour ajuster la taille de la police fonctionnel

--- client/src/app/pages/game-page/board/board.component.ts
+++ client/src/app/pages/game-page/board/board.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ASCII_CODE, Board } from '@app/GameLogic/game/board';
 import { BoardService } from '@app/services/board.service';
 
+const DEFAULT_FONT_SIZE = 24;
+const MAX_FONT_SIZE = 24;
+const MIN_FONT_SIZE = 24;
 @Component({
     selector: 'app-board',
     templateUrl: './board.component.html',
     styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent {
+    @ViewChild('ScrabbleBoard') scrabbleBoard: ElementRef;
     board: Board;
-    fontSize: number = 24;
+    fontSize: number = DEFAULT_FONT_SIZE;
 
     constructor(private boardService: BoardService) {
         this.board = this.boardService.board;
     }
 
     increaseFont(): void {
-        if (this.fontSize <= 24) {
+        if (this.fontSize <= MAX_FONT_SIZE) {
             this.fontSize += 1;
+            (this.scrabbleBoard.nativeElement as HTMLParagraphElement).style.fontSize = `${this.fontSize}px`;
         }
     }
 
     decreaseFont(): void {
-        if (this.fontSize >= 10) {
+        if (this.fontSize >= MIN_FONT_SIZE) {
             this.fontSize -= 1;
+            (this.scrabbleBoard.nativeElement as HTMLParagraphElement).style.fontSize = `${this.fontSize}px`;
         }
     }
 
