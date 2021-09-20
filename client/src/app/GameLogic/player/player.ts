707544c37de1d69bc486e157ef626f25d47079ad
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-20 12:23:36 -0400

    fix exchange letter

--- client/src/app/GameLogic/player/player.ts
+++ client/src/app/GameLogic/player/player.ts
@@ -28,6 +28,35 @@ export abstract class Player {
         console.log(this.letterRack);
     }
 
+    getLettersFromRack(mockLetters: Letter[]): Letter[] {
+        const lettersInRack: Map<string, Letter[]> = new Map();
+        for (const letter of this.letterRack) {
+            const char: string = letter.char;
+            const occcurences: Letter[] | undefined = lettersInRack.get(char);
+            if (occcurences) {
+                occcurences.push(letter);
+            } else {
+                lettersInRack.set(char, [letter]);
+            }
+        }
+
+        const lettersFromRack = [];
+        for (const mockLetter of mockLetters) {
+            const mockChar = mockLetter.char;
+            const lettersLeft = lettersInRack.get(mockChar);
+            if (lettersLeft) {
+                const letterToAdd = lettersLeft.pop();
+                if (!letterToAdd) {
+                    throw Error('Some letters are invalid');
+                }
+                lettersFromRack.push(letterToAdd);
+            } else {
+                throw Error('Some letters are invalid');
+            }
+        }
+        return lettersFromRack;
+    }
+
     get isLetterRackEmpty(): boolean {
         return this.letterRack.length === 0;
     }
