c05439db9cc1d518d0eaa5dd59ef05b6ad084a37
dasadou <65833938+dasadou@users.noreply.github.com>
2021-09-20 15:47:27 -0400

    Le commande parser verifie la validité des paramètres de place Letter

--- client/src/app/GameLogic/commands/command-parser/command-parser.service.ts
+++ client/src/app/GameLogic/commands/command-parser/command-parser.service.ts
@@ -3,6 +3,10 @@ import { Command, CommandType } from '@app/GameLogic/commands/command.interface'
 import { Message } from '@app/GameLogic/messages/message.interface';
 import { Subject } from 'rxjs';
 
+const CHARACTER_V = 'v'.charCodeAt(0);
+const CHARACTER_H = 'h'.charCodeAt(0);
+const MAX_COL = 15;
+const PLACE_LETTER_ARG_SIZE = 4;
 @Injectable({
     providedIn: 'root',
 })
@@ -28,9 +32,32 @@ export class CommandParserService {
         const toVerify = message.content.split(' ');
         const commandCondition = toVerify[0];
         if (commandCondition[0] === '!') {
-            if (Object.values(CommandType).includes(commandCondition as CommandType)) {
+            const commandType = commandCondition as CommandType;
+            if (Object.values(CommandType).includes(commandType)) {
                 const args = toVerify.slice(1, toVerify.length);
                 const command = this.createCommand(args, commandCondition as CommandType);
+                const errorSyntax = 'erreur de syntax';
+                if (commandType === CommandType.Place) {
+                    if (args[0].length === PLACE_LETTER_ARG_SIZE) {
+                        const row = args[0].charCodeAt(0);
+                        const col = Number(args[0][1] + args[0][2]);
+                        const direction = args[0].charCodeAt(3);
+
+                        if (row > 'o'.charCodeAt(0) && row < 'a'.charCodeAt(0)) {
+                            // si depasse 'o' et inferieur a 'a'
+                            throw Error(errorSyntax + ': ligne hors champ');
+                        }
+                        if (col > MAX_COL) {
+                            // superieur a 10
+                            throw Error(errorSyntax + ': colonne hors champ');
+                        }
+                        if (direction !== CHARACTER_H && direction !== CHARACTER_V) {
+                            throw Error(errorSyntax + ': direction invalide');
+                        }
+                    } else {
+                        throw Error(errorSyntax + ': les paramètres sont invalide');
+                    }
+                }
                 this.sendCommand(command);
                 return true;
             }
