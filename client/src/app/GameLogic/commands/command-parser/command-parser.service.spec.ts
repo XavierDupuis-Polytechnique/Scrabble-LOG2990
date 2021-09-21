7c79c9f111a96a6c87df9591e276805534ba0bff
XavierDupuis <xavier0978@hotmail.fr>
2021-09-21 10:26:04 -0400

    remove Tests (default)

--- client/src/app/GameLogic/commands/command-parser/command-parser.service.spec.ts
+++ client/src/app/GameLogic/commands/command-parser/command-parser.service.spec.ts
@@ -1,45 +1,15 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed } from '@angular/core/testing';
+import { CommandParserService } from '@app/GameLogic/commands/command-parser/command-parser.service';
 
-import { inject, TestBed } from '@angular/core/testing';
-import { CommandType } from '@app/GameLogic/commands/command.interface';
-import { Message } from '@app/GameLogic/messages/message.interface';
-import { CommandParserService } from './command-parser.service';
+describe('CommandParserService', () => {
+    let service: CommandParserService;
 
-describe('Service: CommandParser', () => {
-    let messageTestTrue: Message;
-    let fakeMessage: string[];
-    let validCommands: CommandType[];
-    let fakeCommand: CommandType;
-    let messageTestFalse: Message;
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [CommandParserService, CommandType],
-        });
-        validCommands = Object.values(CommandType);
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CommandParserService);
     });
 
-    it('should ...', inject([CommandParserService], (service: CommandParserService) => {
+    it('should be created', () => {
         expect(service).toBeTruthy();
-    }));
-    it('verifyCommand should not return true since there is no message', inject([CommandParserService], (service: CommandParserService) => {
-        expect(service.verifyCommand(messageTestFalse)).toBeUndefined();
-    }));
-    it('createCommand should not return true as the command does not exist', inject([CommandParserService], (service: CommandParserService) => {
-        fakeCommand = CommandType.Help;
-        expect(service.createCommand(fakeMessage, fakeCommand)).toBeUndefined();
-    }));
-
-    // test d'une commande pas rapport
-    it('should not return true as it is not a real command', inject([CommandParserService], (service: CommandParserService) => {
-        messageTestFalse = { content: '!fausseCommande oh non! pas la ceinture, PAS LA CEINTUUUURE!!!', from: 'player 1' };
-        expect(service.verifyCommand(messageTestFalse)).toBeFalse();
-    }));
-    // test d'une commande vide
-    // fonction lambda. throw error
-    it('should return true as they are all real', inject([CommandParserService], (service: CommandParserService) => {
-        validCommands.forEach((command) => {
-            messageTestTrue = { content: command, from: 'playa' };
-            expect(service.verifyCommand(messageTestTrue)).toBeTruthy();
-        });
-    }));
+    });
 });
