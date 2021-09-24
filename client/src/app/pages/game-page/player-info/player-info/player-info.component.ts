178e3f9c525a52ba3e548368c126560c28c0c429
XavierDupuis <xavier0978@hotmail.fr>
2021-09-23 23:34:44 -0400

    "Passer" turn UI button (with disabled/enabled)

--- client/src/app/pages/game-page/player-info/player-info/player-info.component.ts
+++ client/src/app/pages/game-page/player-info/player-info/player-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionValidatorService } from '@app/GameLogic/actions/action-validator.service';
+import { PassTurn } from '@app/GameLogic/actions/pass-turn';
+import { GameInfoService } from '@app/GameLogic/game/game-info/game-info.service';
 import { GameManagerService } from '@app/GameLogic/game/games/game-manager.service';
 
 @Component({
@@ -7,11 +10,21 @@ import { GameManagerService } from '@app/GameLogic/game/games/game-manager.servi
     styleUrls: ['./player-info.component.scss'],
 })
 export class PlayerInfoComponent implements OnInit {
-    constructor(private gameManager: GameManagerService) {}
+    // isItMyTurn: boolean;
+
+    constructor(private gameManager: GameManagerService, private info: GameInfoService, private avs: ActionValidatorService) {}
 
     ngOnInit() {}
 
     abandonner(): void {
         this.gameManager.stopGame();
+    }
+
+    passer() {
+        this.avs.sendAction(new PassTurn(this.info.user));
+    }
+
+    get isItMyTurn() {
+        return this.info.user === this.info.activePlayer;
     }
 }
