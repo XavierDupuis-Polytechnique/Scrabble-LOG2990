901be10dd268ac14f043258c27320a3655fffebd
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-24 21:04:07 -0400

    wordSearcher to service

--- client/src/app/GameLogic/validator/word-search/word-searcher.service.ts
+++ client/src/app/GameLogic/validator/word-search/word-searcher.service.ts
@@ -1,23 +1,25 @@
+import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { Direction } from '@app/GameLogic/actions/direction.enum';
 import { PlaceLetter } from '@app/GameLogic/actions/place-letter';
 import { LetterCreator } from '@app/GameLogic/game/letter-creator';
 import { Tile } from '@app/GameLogic/game/tile';
+import { DictionaryService } from '@app/GameLogic/validator/dictionary.service';
 import { BoardService } from '@app/services/board.service';
-import { DictionaryService } from '../dictionary.service';
 
 const BOARD_MIN_POSITION_X = 0;
 const BOARD_MIN_POSITION_Y = 0;
 const BOARD_MAX_POSITION_X = 15;
 const BOARD_MAX_POSITION_Y = 15;
 
+@Injectable({
+    providedIn: 'root',
+})
 export class WordSearcher {
     listOfValidWord: Tile[][] = [];
-    letterCreator: LetterCreator;
+    letterCreator = new LetterCreator();
     isValid: boolean = false;
-    constructor(private boardService: BoardService, private dictionaryService: DictionaryService, letterCreator: LetterCreator) {
-        this.letterCreator = letterCreator;
-    }
+    constructor(private boardService: BoardService, private dictionaryService: DictionaryService) {}
 
     get grid() {
         return this.boardService.board.grid;
@@ -28,8 +30,6 @@ export class WordSearcher {
     }
 
     validateWords(action: PlaceLetter): boolean {
-        console.log("ACTION:", action);
-
         if (this.dictionaryService.isWordInDict(action.word)) {
             const lettersToPlace = this.findCoordOfLettersToPLace(action);
             for (const letter of lettersToPlace) {
@@ -45,13 +45,13 @@ export class WordSearcher {
 
     goToBeginningOfWord(action: PlaceLetter, pos: Vec2): void {
         const currentPos: Vec2 = { x: pos.x, y: pos.y };
-        if (action.placement.direction === Direction.Horizontal) {
+        const direction = action.placement.direction;
+        if (direction === Direction.Horizontal) {
             while (this.tileIsOccupied(pos.x, pos.y) || this.isLetterPosition(pos, currentPos)) {
                 pos.y -= 1;
             }
             pos.y += 1;
-        }
-        else {
+        } else {
             while (this.tileIsOccupied(pos.x, pos.y) || this.isLetterPosition(pos, currentPos)) {
                 pos.x -= 1;
             }
@@ -61,20 +61,19 @@ export class WordSearcher {
 
     goToEndOfWord(action: PlaceLetter, pos: Vec2, letterPos: Vec2): Tile[] {
         const currentPos = { x: letterPos.x, y: letterPos.y };
-        let word: Tile[] = [];
-        if (action.placement.direction === Direction.Horizontal) {
+        const word: Tile[] = [];
+        const direction = action.placement.direction;
+        if (direction === Direction.Horizontal) {
             while (this.tileIsOccupied(pos.x, pos.y) || this.isLetterPosition(pos, currentPos)) {
                 if (this.tileIsOccupied(pos.x, pos.y)) {
-                    word.push(this.grid[pos.y][pos.x])
-                }
-                else {
+                    word.push(this.grid[pos.y][pos.x]);
+                } else {
                     word.push(this.createTile(action.word[pos.x - action.placement.x], pos));
                 }
                 pos.y += 1;
             }
             pos.y -= 1;
-        }
-        else {
+        } else {
             while (this.tileIsOccupied(pos.x, pos.y) || this.isLetterPosition(pos, currentPos)) {
                 if (this.tileIsOccupied(pos.x, pos.y)) {
                     word.push(this.grid[pos.y][pos.x]);
@@ -86,12 +85,6 @@ export class WordSearcher {
             pos.x -= 1;
         }
         return word;
-    }
-
-    private createTile(char: string, pos: Vec2): Tile {
-        let tile = new Tile(this.grid[pos.y][pos.x].letterMultiplicator, this.grid[pos.y][pos.x].wordMultiplicator);
-        tile.letterObject = this.letterCreator.createLetter(char);
-        return tile;
     }
 
     addWord(word: Tile[]): boolean {
@@ -137,8 +130,8 @@ export class WordSearcher {
     }
 
     findCoordOfLettersToPLace(action: PlaceLetter): Vec2[] {
-        let listOfCoord: Vec2[] = [];
-        let coord: Vec2 = { x: action.placement.x, y: action.placement.y };
+        const listOfCoord: Vec2[] = [];
+        const coord: Vec2 = { x: action.placement.x, y: action.placement.y };
 
         for (let i = 0; i < action.word.length; i++) {
             if (!this.tileIsOccupied(coord.x, coord.y)) {
@@ -151,5 +144,11 @@ export class WordSearcher {
             }
         }
         return listOfCoord;
+    }
+
+    private createTile(char: string, pos: Vec2): Tile {
+        const tile = new Tile(this.grid[pos.y][pos.x].letterMultiplicator, this.grid[pos.y][pos.x].wordMultiplicator);
+        tile.letterObject = this.letterCreator.createLetter(char);
+        return tile;
     }
 }
