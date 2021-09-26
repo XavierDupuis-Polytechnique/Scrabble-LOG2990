7b1b8c3176fd4a0d8a06dabb95f69af3d04dfa03
Julie-anne <jaeggi.ja@gmail.com>
2021-09-25 23:52:06 -0400

    Word search update - refactor function

--- client/src/app/GameLogic/validator/word-search/word-searcher.service.spec.ts
+++ client/src/app/GameLogic/validator/word-search/word-searcher.service.spec.ts
@@ -13,14 +13,14 @@ import { DictionaryService } from '@app/GameLogic/validator/dictionary.service';
 import { WordSearcher } from '@app/GameLogic/validator/word-search/word-searcher.service';
 import { BoardService } from '@app/services/board.service';
 
-const BOARD_LENGTH = 6;
-const BOARD_WIDTH = 6;
+const BOARD_LENGTH = 10;
+const BOARD_WIDTH = 10;
 class MockDictionaryService extends DictionaryService {
     @Injectable()
     mockDictionary: Dictionary = {
         title: 'dictionnaire',
         description: 'mots',
-        words: ['bateau', 'butte', 'allo', 'ou', 'oui', 'nil', 'ni', 'on'],
+        words: ['bateau', 'butte', 'allo', 'ou', 'oui', 'nil', 'ni', 'on', 'bon'],
     };
 
     isWordInDict(word: string): boolean {
@@ -53,7 +53,7 @@ class MockBoard extends Board {
 
         this.grid[1][3].letterObject = { char: 'O', value: 1 };
 
-        this.grid[2][3].letterObject = { char: 'U', value: 1 };
+        this.grid[2][3].letterObject = { char: 'N', value: 1 };
 
 
 
@@ -67,7 +67,7 @@ class MockBoard extends Board {
 
 describe('WordSearcher', () => {
     let wordSearcher: WordSearcher;
-    let mockBoard: MockBoard = new MockBoard();
+    let mockBoard: MockBoard;
     let dictionaryService: MockDictionaryService;
     let boardService: BoardService;
 
@@ -79,6 +79,7 @@ describe('WordSearcher', () => {
         });
         boardService = TestBed.inject(BoardService);
         dictionaryService = TestBed.inject(MockDictionaryService);
+        mockBoard = new MockBoard();
         boardService.board = mockBoard;
         wordSearcher = new WordSearcher(boardService, dictionaryService);
     });
@@ -101,57 +102,56 @@ describe('WordSearcher', () => {
         const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
         const action = new PlaceLetter(player, 'oui', placement);
         const position: Vec2 = { x: 4, y: 1 };
-        const beginPosition = wordSearcher.goToBeginningOfWord(action.placement.direction, position);
+        const direction = action.placement.direction as Direction;
+        const beginPosition = wordSearcher.goToBeginningOfWord(direction, position);
         expect(beginPosition).toEqual({ x: 4, y: 0 });
     });
 
-    // it('should go to end of word', () => {
-    //     // Nil
-    //     mockBoard.grid[2][5].letterObject = { char: 'L', value: 1 };
+    it('should go to end of word and return the word made', () => {
+        // Nil
+        mockBoard.grid[2][5].letterObject = { char: 'L', value: 1 };
+        const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
+        const action = new PlaceLetter(player, 'oui', placement);
+        const beginingPosition: Vec2 = { x: 5, y: 0 };
+        const letterPosition: Vec2 = { x: 5, y: 1 };
+        const endLetter = wordSearcher.goToEndOfWord(action, beginingPosition, letterPosition);
+        expect(wordSearcher.tileToString(endLetter)).toEqual('NIL');
+    });
 
-    //     const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
-    //     const action = new PlaceLetter(player, 'oui', placement);
-    //     const beginingPosition: Vec2 = { x: 5, y: 0 };
-    //     const letterPosition: Vec2 = { x: 5, y: 1 };
+    it('should find 0 neighbour if first word ', () => {
+        const placement: PlacementSetting = { x: 4, y: 0, direction: Direction.Horizontal };
+        const hasNeighbour = wordSearcher.hasNeighbour({ x: placement.x, y: placement.y }, placement.direction as Direction);
+        expect(hasNeighbour).toBe(false);
 
-    //     const endLetter = wordSearcher.goToEndOfWord(action, beginingPosition, letterPosition);
-    //     expect(wordSearcher.tileToString(endLetter)).toEqual('L');
-    // });
+    });
 
-    // it('should find 0 neighbour if first word ', () => {
-    //     const placement: PlacementSetting = { x: 4, y: 0, direction: Direction.Horizontal };
-    //     const hasNeighbour = wordSearcher.hasNeighbour({ x: placement.x, y: placement.y });
-    //     expect(hasNeighbour).toBe(false);
+    it('should validate word if first word ', () => {
+        const placement: PlacementSetting = { x: 4, y: 0, direction: Direction.Horizontal };
+        const action = new PlaceLetter(player, 'on', placement);
+        const wordIsValid = wordSearcher.validateWords(action);
+        expect(wordIsValid).toBe(true);
 
-    // });
+    });
 
-    // it('should validate word if first word ', () => {
-    //     const placement: PlacementSetting = { x: 4, y: 0, direction: Direction.Horizontal };
-    //     const action = new PlaceLetter(player, 'on', placement);
-    //     const wordIsValid = wordSearcher.validateWords(action);
-    //     expect(wordIsValid).toBe(true);
+    it('should find all valid words', () => {
+        // On ajoute le mot oui a la poisition (3,1) on a u(1,3);
 
-    // });
+        const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
+        const action = new PlaceLetter(player, 'oui', placement);
+        const validWord = wordSearcher.listOfValidWord(action);
+        expect(wordSearcher.tileToString(validWord[0])).toEqual('OUI');
+        expect(wordSearcher.tileToString(validWord[1])).toEqual('OU');
+        expect(wordSearcher.tileToString(validWord[2])).toEqual('NI');
 
-    // it('should find one neighbor', () => {
-    //     // On ajoute le mot oui a la poisition (3,1) on a u(1,3);
-
-    //     const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
-    //     const action = new PlaceLetter(player, 'oui', placement);
-    //     const validWord = wordSearcher.listOfValidWord(action);
-    //     expect(wordSearcher.tileToString(validWord[0])).toEqual('OU');
-
-    // });
-
-    // it('should find all neighbors', () => {
-    //     // On ajoute le mot oui a la poisition (3,1) on a u(1,3);
-    //     mockBoard.grid[2][5].letterObject = { char: 'L', value: 1 };
-    //     const placement: PlacementSetting = { x: 3, y: 1, direction: Direction.Horizontal };
-    //     const action = new PlaceLetter(player, 'oui', placement);
-    //     const validWord = wordSearcher.listOfValidWord(action);
-    //     expect(wordSearcher.tileToString(validWord[0]).toLowerCase()).toContain('ou');
-    //     expect(wordSearcher.tileToString(validWord[1]).toLowerCase()).toContain('nil');
-
-    // });
-
+    });
+    it('should find all neighbors', () => {
+        // On ajoute le mot oui a la poisition (3,1) on a u(1,3);
+        mockBoard.grid[2][2].letterObject = { char: 'O', value: 1 };
+        const placement: PlacementSetting = { x: 1, y: 2, direction: Direction.Vertical };
+        const action = new PlaceLetter(player, 'bateau', placement);
+        const validWord = wordSearcher.listOfValidWord(action);
+        console.log('VALID:', validWord);
+        expect(wordSearcher.tileToString(validWord[0])).toEqual('BATEAU');
+        expect(wordSearcher.tileToString(validWord[1])).toEqual('BON');
+    });
 });
--- client/src/app/GameLogic/validator/word-search/word-searcher.service.ts
+++ client/src/app/GameLogic/validator/word-search/word-searcher.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { Direction } from '@app/GameLogic/actions/direction.enum';
-import { PlaceLetter } from '@app/GameLogic/actions/place-letter';
+import { PlaceLetter, PlacementSetting } from '@app/GameLogic/actions/place-letter';
 import { LetterCreator } from '@app/GameLogic/game/letter-creator';
 import { Tile } from '@app/GameLogic/game/tile';
 import { DictionaryService } from '@app/GameLogic/validator/dictionary.service';
@@ -9,8 +9,8 @@ import { BoardService } from '@app/services/board.service';
 
 const BOARD_MIN_POSITION_X = 0;
 const BOARD_MIN_POSITION_Y = 0;
-const BOARD_MAX_POSITION_X = 15;
-const BOARD_MAX_POSITION_Y = 15;
+const BOARD_MAX_POSITION_X = 14;
+const BOARD_MAX_POSITION_Y = 14;
 
 @Injectable({
     providedIn: 'root',
@@ -30,16 +30,18 @@ export class WordSearcher {
             return true;
         }
         return false;
-
     }
 
     listOfValidWord(action: PlaceLetter): Tile[][] {
         const listOfValidWord: Tile[][] = [];
         if (this.dictionaryService.isWordInDict(action.word)) {
-            if (this.hasNeighbour({ x: action.placement.x, y: action.placement.y })) {
-                const coordsOfLettersToPlace = this.findCoordOfLettersToPLace(action);
-                for (const coord of coordsOfLettersToPlace) {
-                    const direction = action.placement.direction;
+            const wordInTile = this.stringToTile(action.word, action.placement);
+            listOfValidWord.push(wordInTile);
+
+            const coordsOfLettersToPlace = this.findCoordOfLettersToPLace(action);
+            for (const coord of coordsOfLettersToPlace) {
+                const direction = action.placement.direction as Direction;
+                if (this.hasNeighbour(coord, direction)) {
                     const beginingPos = this.goToBeginningOfWord(direction, coord);
                     const word = this.goToEndOfWord(action, beginingPos, coord);
                     if (this.isValid(word)) {
@@ -47,79 +49,61 @@ export class WordSearcher {
                     }
                 }
             }
+
         }
         return listOfValidWord;
     }
 
-    hasNeighbour(coord: Vec2): boolean {
+    hasNeighbour(coord: Vec2, direction: Direction): boolean {
         const x = coord.x;
         const y = coord.y;
-        if (x + 1 < BOARD_MAX_POSITION_X) {
-            if (this.boardService.board.grid[x + 1][y].letterObject.char !== ' ') {
-                return true;
-            }
+        if (direction === Direction.Horizontal) {
+            return this.tileIsOccupied(x, y + 1) || this.tileIsOccupied(x, y - 1);
+        } else {
+            return this.tileIsOccupied(x + 1, y) || this.tileIsOccupied(x - 1, y);
         }
-        if (x - 1 >= 0) {
-            if (this.boardService.board.grid[x - 1][y].letterObject.char !== ' ') {
-                return true;
-            }
-        }
-        if (y + 1 < BOARD_MAX_POSITION_Y) {
-            if (this.boardService.board.grid[x][y + 1].letterObject.char !== ' ') {
-                return true;
-            }
-        }
-        if (y - 1 >= 0) {
-            if (this.boardService.board.grid[x][y - 1].letterObject.char !== ' ') {
-                return true;
-            }
-        }
-        return false;
     }
 
-    goToBeginningOfWord(direction: string, letterPos: Vec2): Vec2 {
-        const currentPos: Vec2 = { ...letterPos };
-        let x = currentPos.x;
-        let y = currentPos.y;
-
+    goToBeginningOfWord(direction: Direction, letterPos: Vec2): Vec2 {
+        let x = letterPos.x;
+        let y = letterPos.y;
         if (direction as Direction === Direction.Horizontal) {
-            while (this.tileIsOccupied(x, y) || this.isLetterPosition(currentPos, letterPos)) {
+            while (this.tileIsOccupied(x, y) || this.isLetterPosition({ x, y }, letterPos)) {
                 y -= 1;
             }
             y += 1;
         } else {
-            while (this.tileIsOccupied(x, y) || this.isLetterPosition(currentPos, letterPos)) {
+            while (this.tileIsOccupied(x, y) || this.isLetterPosition({ x, y }, letterPos)) {
                 x -= 1;
             }
             x += 1;
         }
-        return currentPos;
+        return { x, y };
     }
 
     goToEndOfWord(action: PlaceLetter, beginingPos: Vec2, letterPos: Vec2): Tile[] {
-        const currentPos = { ...beginingPos };
         const direction = action.placement.direction;
-        let x = currentPos.x;
-        let y = currentPos.y;
+        let x = beginingPos.x;
+        let y = beginingPos.y;
         const word: Tile[] = [];
         if (direction === Direction.Horizontal) {
-            while (this.tileIsOccupied(x, y) || this.isLetterPosition(currentPos, letterPos)) {
+            while (this.tileIsOccupied(x, y) || this.isLetterPosition({ x, y }, letterPos)) {
                 if (this.tileIsOccupied(x, y)) {
                     word.push(this.grid[y][x]);
                 } else {
                     const index = x - action.placement.x;
-                    word.push(this.createTile(action.word[index], currentPos));
+                    word.push(this.createTile(action.word[index], { x, y }));
                 }
                 y += 1;
             }
             y -= 1;
         } else {
-            while (this.tileIsOccupied(x, y) || this.isLetterPosition(currentPos, letterPos)) {
+            while (this.tileIsOccupied(x, y) || this.isLetterPosition({ x, y }, letterPos)) {
                 if (this.tileIsOccupied(x, y)) {
                     word.push(this.grid[y][x]);
                 } else {
                     const index = y - action.placement.y;
-                    word.push(this.createTile(action.word[index], currentPos));
+                    word.push(this.createTile(action.word[index], { x, y }));
                 }
                 x += 1;
             }
@@ -143,20 +127,19 @@ export class WordSearcher {
     }
 
     isInsideBoard(x: number, y: number): boolean {
-        if (x >= BOARD_MIN_POSITION_X && y >= BOARD_MIN_POSITION_Y && x <= BOARD_MAX_POSITION_X && y <= BOARD_MAX_POSITION_Y) {
-            return true;
-        }
-        return false;
+        return x >= BOARD_MIN_POSITION_X
+            && y >= BOARD_MIN_POSITION_Y
+            && x <= BOARD_MAX_POSITION_X
+            && y <= BOARD_MAX_POSITION_Y;
     }
 
     tileIsOccupied(x: number, y: number): boolean {
         if (!this.isInsideBoard(x, y)) {
             return false;
         }
-        if (this.grid[y][x].letterObject.char === ' ') {
-            return false;
-        }
-        return true;
+        const char = this.grid[y][x].letterObject.char;
+        return char !== ' ';
+
     }
 
     findCoordOfLettersToPLace(action: PlaceLetter): Vec2[] {
@@ -182,6 +165,22 @@ export class WordSearcher {
             }
         }
         return listOfCoord;
+    }
+
+    stringToTile(word: string, placement: PlacementSetting): Tile[] {
+        let x = placement.x;
+        let y = placement.y;
+        let wordTile: Tile[] = [];
+        for (const letter of word) {
+            let tile = this.createTile(letter, { x, y })
+            wordTile.push(tile);
+            if (placement.direction === Direction.Horizontal) {
+                x++;
+            } else {
+                y++;
+            }
+        }
+        return wordTile;
     }
 
     private createTile(char: string, pos: Vec2): Tile {
