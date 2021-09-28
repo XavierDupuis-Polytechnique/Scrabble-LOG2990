d9a079af3d77c6a8e4c304dcb2f7077fba0bf88d
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-28 13:48:47 -0400

    fix horse crash when no game

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -8,13 +8,10 @@ import { GameManagerService } from '@app/GameLogic/game/games/game-manager.servi
     styleUrls: ['./game-page.component.scss'],
 })
 export class GamePageComponent implements OnInit {
-    constructor(private gameManager: GameManagerService, private router: Router) {}
-
-    ngOnInit() {
+    constructor(private gameManager: GameManagerService, private router: Router) {
         try {
             this.gameManager.startGame();
         } catch (e) {
-            // alert("Pas de partie crée pour l'instant");
             this.router.navigate(['/']);
         }
     }
