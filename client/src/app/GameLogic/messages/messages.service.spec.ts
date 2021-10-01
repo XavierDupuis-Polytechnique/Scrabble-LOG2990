64479001341fa4d85338e75650b4a0a6eef998f4
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-30 21:03:13 -0400

    wip message  service test

--- client/src/app/GameLogic/messages/messages.service.spec.ts
+++ client/src/app/GameLogic/messages/messages.service.spec.ts
@@ -1,16 +1,31 @@
 /* tslint:disable:no-unused-variable */
 
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { Message, MessageType } from '@app/GameLogic/messages/message.interface';
 import { MessagesService } from './messages.service';
 
 describe('Service: Messages', () => {
+    let service: MessagesService;
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [MessagesService],
-        });
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MessagesService);
     });
 
-    it('should ...', inject([MessagesService], (service: MessagesService) => {
+    it('should create message', () => {
         expect(service).toBeTruthy();
-    }));
+    });
+
+    it('should receive message', () => {
+        const from = 'paul';
+        const content = 'allo';
+        service.receiveMessage(from, content);
+        const logs = service.messagesLog;
+        const lastMessage = logs[logs.length - 1];
+        const expectedMesssage: Message = {
+            from,
+            content,
+            type: MessageType.Player1,
+        };
+        expect(lastMessage).toEqual(expectedMesssage);
+    });
 });
