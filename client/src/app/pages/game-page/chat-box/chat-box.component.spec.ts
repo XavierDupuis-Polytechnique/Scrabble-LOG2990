f24399ce1de673d0194e92bb19a493521b9e4cea
XavierDupuis <xavier0978@hotmail.fr>
2021-10-01 08:58:52 -0400

    fix messages tests

--- client/src/app/pages/game-page/chat-box/chat-box.component.spec.ts
+++ client/src/app/pages/game-page/chat-box/chat-box.component.spec.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectorRef } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { GameInfoService } from '@app/GameLogic/game/game-info/game-info.service';
-import { MessageType, Message } from '@app/GameLogic/messages/message.interface';
+import { Message, MessageType } from '@app/GameLogic/messages/message.interface';
 import { MessagesService } from '@app/GameLogic/messages/messages.service';
 import { User } from '@app/GameLogic/player/user';
 import { BehaviorSubject } from 'rxjs';
-
 import { ChatBoxComponent } from './chat-box.component';
 
 describe('ChatBoxComponent', () => {
@@ -16,7 +15,7 @@ describe('ChatBoxComponent', () => {
     let cdRefSpy: jasmine.SpyObj<ChangeDetectorRef>;
 
     beforeEach(() => {
-        messageServiceSpy = jasmine.createSpyObj('MessagesService', ['receiveMessage']);
+        messageServiceSpy = jasmine.createSpyObj('MessagesService', ['receiveMessagePlayer']);
         messageServiceSpy.messages$ = new BehaviorSubject<Message[]>([{ content: 'Test', from: 'test from', type: MessageType.Player1 }]);
         gameInfoServiceSpy = jasmine.createSpyObj('GameInfoService', ['getPlayer']);
         gameInfoServiceSpy.user = new User('SAMUEL');
@@ -49,11 +48,11 @@ describe('ChatBoxComponent', () => {
     it('should send message to message service if message valid', () => {
         component.messageForm.setValue('Test message');
         component.sendMessage();
-        expect(messageServiceSpy.receiveMessage).toHaveBeenCalled();
+        expect(messageServiceSpy.receiveMessagePlayer).toHaveBeenCalled();
     });
 
     it('should not send a message if message is not valid', () => {
         component.sendMessage();
-        expect(messageServiceSpy.receiveMessage.calls.count()).toBe(0);
+        expect(messageServiceSpy.receiveMessagePlayer.calls.count()).toBe(0);
     });
 });
