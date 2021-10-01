738af0ac7a758ad19e1ccf77b150437c05c0a5e4
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-09-30 23:22:27 -0400

    message service test

--- client/src/app/GameLogic/messages/messages.service.spec.ts
+++ client/src/app/GameLogic/messages/messages.service.spec.ts
@@ -1,13 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed } from '@angular/core/testing';
+import { CommandParserService } from '@app/GameLogic/commands/command-parser/command-parser.service';
 import { Message, MessageType } from '@app/GameLogic/messages/message.interface';
 import { MessagesService } from './messages.service';
 
 describe('Service: Messages', () => {
     let service: MessagesService;
+    let commandParserSpy: jasmine.SpyObj<CommandParserService>;
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        commandParserSpy = jasmine.createSpyObj('CommandParserService', ['parse']);
+        TestBed.configureTestingModule({
+            providers: [{ provide: CommandParserService, useValue: commandParserSpy }],
+        });
         service = TestBed.inject(MessagesService);
     });
 
@@ -27,5 +32,85 @@ describe('Service: Messages', () => {
             type: MessageType.Player1,
         };
         expect(lastMessage).toEqual(expectedMesssage);
+    });
+
+    it('should receive system message', () => {
+        const content = 'test';
+        service.receiveSystemMessage(content);
+        const expectedMesssage: Message = {
+            from: 'System',
+            content,
+            type: MessageType.System,
+        };
+        const log = service.messagesLog;
+        const lastMessage = log[log.length - 1];
+        expect(lastMessage).toEqual(expectedMesssage);
+    });
+
+    it('should receive error', () => {
+        const errorContent = 'this is an error';
+        const error = new Error(errorContent);
+        service.receiveError(error);
+        const log = service.messagesLog;
+        const lastMessage = log[log.length - 1];
+        const message: Message = {
+            from: 'SystemError',
+            content: errorContent,
+            type: MessageType.System,
+        };
+        expect(lastMessage).toEqual(message);
+    });
+
+    it('should catch error when not valid command', () => {
+        const errorContent = 'this is a parse error';
+        commandParserSpy.parse.and.throwError(errorContent);
+        const content = '!notACommand';
+        const from = 'tom';
+        service.receiveMessage(from, content);
+        const log = service.messagesLog;
+        const lastMessage = log[log.length - 1];
+        const errorMessage: Message = {
+            from: 'SystemError',
+            content: errorContent,
+            type: MessageType.System,
+        };
+        expect(lastMessage).toEqual(errorMessage);
+    });
+
+    it('should receive error Message', () => {
+        const errorContent = 'this is a parse error';
+        service.receiveErrorMessage(errorContent);
+        const log = service.messagesLog;
+        const lastMessage = log[log.length - 1];
+        const message: Message = {
+            from: 'SystemError',
+            content: errorContent,
+            type: MessageType.System,
+        };
+        expect(lastMessage).toEqual(message);
+    });
+
+    it('should receive error Message', () => {
+        const errorContent = 'this is a parse error';
+        service.receiveErrorMessage(errorContent);
+        const log = service.messagesLog;
+        const lastMessage = log[log.length - 1];
+        const message: Message = {
+            from: 'SystemError',
+            content: errorContent,
+            type: MessageType.System,
+        };
+        expect(lastMessage).toEqual(message);
+    });
+
+    it('should clear log', () => {
+        service.receiveMessage('tim', 'to be');
+        service.receiveMessage('cook', 'or');
+        service.receiveMessage('apple', 'not to be');
+        const prevNLogs = service.messagesLog.length;
+        expect(prevNLogs).toBe(3);
+        service.clearLog();
+        const nLogs = service.messagesLog.length;
+        expect(nLogs).toBe(0);
     });
 });
