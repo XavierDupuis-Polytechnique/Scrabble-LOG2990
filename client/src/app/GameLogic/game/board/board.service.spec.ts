8a1481c47455708506c56d65297ddc3bad254b3b
Darkforce689 <60223836+Darkforce689@users.noreply.github.com>
2021-10-01 19:46:34 -0400

    BoardService 100%

--- client/src/app/GameLogic/game/board/board.service.spec.ts
+++ client/src/app/GameLogic/game/board/board.service.spec.ts
@@ -1,11 +1,38 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 /* tslint:disable:no-unused-variable */
 
+import { LetterCreator } from '@app/GameLogic/game/board/letter-creator';
 import { BoardService } from './board.service';
 
 describe('board service', () => {
     const boardService = new BoardService();
+    const letterCreator = new LetterCreator();
 
     it('should have property board', () => {
         expect(boardService.board).toBeDefined();
+    });
+
+    it('should x + 1 hasNeighbour', () => {
+        boardService.board.grid[5][5].letterObject = letterCreator.createLetter('A');
+        boardService.hasNeighbour(4, 5);
+        expect(boardService.hasNeighbour(4, 5)).toBeTruthy();
+    });
+
+    it('should x - 1 hasNeighbour', () => {
+        boardService.board.grid[5][5].letterObject = letterCreator.createLetter('A');
+        boardService.hasNeighbour(6, 5);
+        expect(boardService.hasNeighbour(4, 5)).toBeTruthy();
+    });
+
+    it('should y + 1 hasNeighbour', () => {
+        boardService.board.grid[5][5].letterObject = letterCreator.createLetter('A');
+        boardService.hasNeighbour(5, 4);
+        expect(boardService.hasNeighbour(4, 5)).toBeTruthy();
+    });
+
+    it('should y - 1 hasNeighbour', () => {
+        boardService.board.grid[5][5].letterObject = letterCreator.createLetter('A');
+        boardService.hasNeighbour(4, 6);
+        expect(boardService.hasNeighbour(4, 5)).toBeTruthy();
     });
 });
