6049959f1fb4b6f54fa5f28ba682ecdc658cfcc4
Samuel <laporte.samuel2000@gmail.com>
2021-10-01 16:39:00 -0400

    test player-info

--- client/src/app/pages/game-page/player-info/player-info.component.ts
+++ client/src/app/pages/game-page/player-info/player-info.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { ActionValidatorService } from '@app/GameLogic/actions/action-validator.service';
-import { PassTurn } from '@app/GameLogic/actions/pass-turn';
 import { GameInfoService } from '@app/GameLogic/game/game-info/game-info.service';
-import { GameManagerService } from '@app/GameLogic/game/games/game-manager.service';
 
 @Component({
     selector: 'app-player-info',
@@ -10,29 +7,5 @@ import { GameManagerService } from '@app/GameLogic/game/games/game-manager.servi
     styleUrls: ['./player-info.component.scss'],
 })
 export class PlayerInfoComponent {
-    constructor(private gameManager: GameManagerService, public info: GameInfoService, private avs: ActionValidatorService) {}
-
-    abandonner(): void {
-        this.gameManager.stopGame();
-    }
-
-    passer() {
-        this.avs.sendAction(new PassTurn(this.info.user));
-    }
-
-    get isItMyTurn() {
-        try {
-            return this.info.user === this.info.activePlayer;
-        } catch (e) {
-            return false;
-        }
-    }
-
-    get isEndOfGame() {
-        try {
-            return this.info.isEndOfGame;
-        } catch (e) {
-            return false;
-        }
-    }
+    constructor(public info: GameInfoService) {}
 }
