daa62ea7c3e2573c7c24de60ded634dc4365366f
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-10-13 19:23:04 -0400

    wip room chat

--- server/app/messagesService/service/messages.service.ts
+++ server/app/messagesService/service/messages.service.ts
@@ -1,11 +1,14 @@
 import * as io from 'socket.io';
 import * as http from 'http';
 import { Room } from '@app/messagesService/service/room';
+import { Message } from '@app/messagesService/service/message.interface';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import { ChatUser } from '@app/messagesService/service/chat-user.interface';
 
 export class MessageHandler {
     private sio: io.Server;
-    private rooms = new Map<string, Room>();
-    private users = new Map<string, string>();
+    private activeRooms = new Map<string, Room>();
+    private users = new Map<string, ChatUser>();
 
     constructor(server: http.Server) {
         this.sio = new io.Server(server, {
@@ -20,24 +23,40 @@ export class MessageHandler {
             socket.on('userName', (userName: string) => {
                 const id = socket.id;
                 if (!this.users.has(id)) {
-                    this.users.set(id, userName);
+                    const newUser: ChatUser = {
+                        name: userName
+                    }
+                    this.users.set(id, newUser);
+                } else {
+                    this.sendError(socket, 'userName already picked');
                 }
                 console.log('userName', this.users);
             });
 
-            socket.on('newMessage', (content: string) => {
-                const rooms = socket.rooms;
+            socket.on('roomMessages', (content: string) => {
                 const id = socket.id;
-                this.addMessageToRoom(id, content);
-                console.log('message rooms', rooms);
+                try {
+                    this.addMessageToRoom(id, content);
+                } catch (e) {
+                    this.sendError(socket, (e as Error).message);
+                }
             });
 
             socket.on('joinRoom', (roomID: string) => {
-                if (!this.rooms.has(roomID)) {
+                if (!this.activeRooms.has(roomID)) {
                     this.createRoom(roomID);
                 }
-                socket.join(roomID);
-                console.log('rooms', this.rooms);
+                const id = socket.id;
+                const user = this.users.get(id);
+                if (user) {
+                    if (!user.currentRoom) {
+                        this.sendError(socket, 'You have already joined a room');
+                    } else {
+                        user.currentRoom = roomID;
+                        socket.join(roomID);
+                    }
+                }
+                console.log('rooms', this.activeRooms);
             });
             socket.emit('hello', 'test');
         });
@@ -49,15 +68,38 @@ export class MessageHandler {
     }
 
     private addMessageToRoom(socketID: string, content: string): void {
-        const userName = this.users.get(socketID);
+        const user = this.users.get(socketID);
+        if (!user) {
+            throw Error('You have not entered a name in our system');
+        }
+        const userName = user.name;
+        const message: Message = {
+            from: userName,
+            content,
+        };
         console.log(userName, ':', content);
+
+        const roomID = user.currentRoom;
+        if (!roomID) {
+            throw Error('No chat room joined');
+        }
+        const room = this.activeRooms.get(roomID);
+        if (!room) {
+            throw Error('Room not active anymore');
+        }
+        this.sio.to(roomID).emit('roomMessages', message);
+        room.addMessage(message);
     }
 
     private createRoom(roomID: string): void {
-        this.rooms.set(roomID, new Room());
+        this.activeRooms.set(roomID, new Room());
     }
 
     private deleteUser(socketID: string): void {
         this.users.delete(socketID);
+    }
+
+    private sendError(socket: io.Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>, errorMessage: string) {
+        socket.emit('error', errorMessage);
     }
 }
