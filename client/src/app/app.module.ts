137f949898646ea05e7c95491a8d5c5da6a991f1
Samuel <laporte.samuel2000@gmail.com>
2021-10-19 15:22:27 -0400

    fix test canvas
    Ajout des bons tests pour le canvasDrawer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { NewSoloGameFormComponent } from './components/new-solo-game-form/new-so
 import { GameLogicModule } from './GameLogic/game-logic.module';
 import { ClassicGameComponent } from './pages/classic-game/classic-game.component';
 import { BoardComponent } from './pages/game-page/board/board.component';
-import { TileComponent } from './pages/game-page/board/tile/tile.component';
 import { ChatBoxComponent } from './pages/game-page/chat-box/chat-box.component';
 import { HorseComponent } from './pages/game-page/horse/horse.component';
 import { InfoBoxComponent } from './pages/game-page/info-box/info-box.component';
@@ -48,7 +47,6 @@ import { Log2990GameComponent } from './pages/log2990-game/log2990-game.componen
         Log2990GameComponent,
         LeaderboardComponent,
         HeaderBarComponent,
-        TileComponent,
         BoldPipe,
     ],
     imports: [
