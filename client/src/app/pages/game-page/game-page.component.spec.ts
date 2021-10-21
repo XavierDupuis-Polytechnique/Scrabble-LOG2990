273bfba901cb26b78045c3903c58d9bf17f1f544
dasadou <65833938+dasadou@users.noreply.github.com>
2021-10-21 14:52:05 -0400

    Test supplementaire pour game-page

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -6,7 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { ActionValidatorService } from '@app/GameLogic/actions/action-validator.service';
+import { UIInputControllerService } from '@app/GameLogic/actions/ui-actions/ui-input-controller.service';
 import { GameManagerService } from '@app/GameLogic/game/games/game-manager.service';
+import { InputType, UIInput } from '@app/GameLogic/interface/ui-input';
 import { of } from 'rxjs';
 import { GamePageComponent } from './game-page.component';
 
@@ -15,9 +17,19 @@ describe('GamePageComponent', () => {
     let fixture: ComponentFixture<GamePageComponent>;
     let gameManagerServiceSpy: jasmine.SpyObj<GameManagerService>;
     let cdRefSpy: jasmine.SpyObj<ChangeDetectorRef>;
+    let uiInput: UIInput;
     class ActionValidatorServiceMock {
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         sendAction() {}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+    }
+    class UIInputControllerServiceMock {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        receive() {}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        confirm() {}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        cancel() {}
     }
     class MatDialogMock {
         open() {
@@ -39,18 +51,35 @@ describe('GamePageComponent', () => {
                 { provide: ActionValidatorService, useClass: ActionValidatorServiceMock },
                 { provide: ChangeDetectorRef, useValue: cdRefSpy },
                 { provide: MatDialog, useClass: MatDialogMock },
+                { provide: UIInputControllerService, useClass: UIInputControllerServiceMock },
             ],
         }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(GamePageComponent);
+        uiInput = { type: InputType.LeftClick };
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('A keypress should call receive', () => {
+        // eslint-disable-next-line dot-notation
+        const inputControllerSpy = spyOn(component['inputController'], 'receive');
+        const keyEvent = new KeyboardEvent('keydown', { code: 'KeyA' });
+        component.keypressEvent(keyEvent);
+        expect(inputControllerSpy).toBeTruthy();
+    });
+
+    it('An input should call receive', () => {
+        // eslint-disable-next-line dot-notation
+        const inputControllerSpy = spyOn(component['inputController'], 'receive');
+        component.receiveInput(uiInput);
+        expect(inputControllerSpy).toBeTruthy();
     });
 
     it('confirming to abandon should call method stopgame', () => {
@@ -64,5 +93,19 @@ describe('GamePageComponent', () => {
         const actionValidatorSpy = spyOn(component['avs'], 'sendAction');
         component.pass();
         expect(actionValidatorSpy).toHaveBeenCalled();
+    });
+
+    it('should call confirm', () => {
+        // eslint-disable-next-line dot-notation
+        const inputControllerSpy = spyOn(component['inputController'], 'confirm');
+        component.confirm();
+        expect(inputControllerSpy).toBeTruthy();
+    });
+
+    it('should call cancel', () => {
+        // eslint-disable-next-line dot-notation
+        const inputControllerSpy = spyOn(component['inputController'], 'cancel');
+        component.cancel();
+        expect(inputControllerSpy).toBeTruthy();
     });
 });
