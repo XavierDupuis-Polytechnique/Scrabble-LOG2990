66a803321952ceb2e81713cee9b1855c69a99e6c
dasadou <65833938+dasadou@users.noreply.github.com>
2021-10-28 16:32:38 -0400

    Action compiler terminé
    les tests passent, les commandes sont présentes

--- client/src/app/GameLogic/actions/online-action-compiler.service.ts
+++ client/src/app/GameLogic/actions/online-action-compiler.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Action } from './action';
-import { ActionValidatorService } from './action-validator.service';
 import { ExchangeLetter } from './exchange-letter';
 import { OnlineAction, OnlineActionType } from './online-action-compiler.interface';
 import { PassTurn } from './pass-turn';
@@ -10,26 +9,22 @@ import { PlaceLetter } from './place-letter';
     providedIn: 'root',
 })
 export class OnlineActionCompilerService {
-    constructor(private actionValidator: ActionValidatorService) {}
-
-    compileActionOnline(action: Action) {
-        if (!this.actionValidator.validateAction(action)) {
-            return;
-        }
+    compileActionOnline(action: Action): OnlineAction | undefined {
         if (action instanceof PlaceLetter) {
-            this.compilePlaceLetterOnline(action);
+            return this.compilePlaceLetterOnline(action);
         }
 
         if (action instanceof ExchangeLetter) {
-            this.compileExchangeLetterOnline(action);
+            return this.compileExchangeLetterOnline(action);
         }
 
         if (action instanceof PassTurn) {
-            this.compilePassTurnOnline();
+            return this.compilePassTurnOnline();
         }
+        return undefined;
     }
 
-    compilePlaceLetterOnline(action: PlaceLetter): OnlineAction {
+    private compilePlaceLetterOnline(action: PlaceLetter): OnlineAction {
         const onlinePlaceLetter: OnlineAction = {
             type: OnlineActionType.Place,
             placementSettings: action.placement,
@@ -38,7 +33,7 @@ export class OnlineActionCompilerService {
         return onlinePlaceLetter;
     }
 
-    compileExchangeLetterOnline(action: ExchangeLetter): OnlineAction {
+    private compileExchangeLetterOnline(action: ExchangeLetter): OnlineAction {
         let lettersToExchange = '';
         action.lettersToExchange.forEach((letter) => {
             lettersToExchange += letter.char;
@@ -50,7 +45,7 @@ export class OnlineActionCompilerService {
         return onlineExchangeLetter;
     }
 
-    compilePassTurnOnline(): OnlineAction {
+    private compilePassTurnOnline(): OnlineAction {
         const passTurn: OnlineAction = {
             type: OnlineActionType.Pass,
         };
