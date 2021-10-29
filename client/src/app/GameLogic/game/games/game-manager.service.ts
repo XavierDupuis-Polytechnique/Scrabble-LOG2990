7be570ba50476cadb28788b69d747f75d1d6f8b4
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-10-28 20:56:08 -0400

    feat(online-chat-client): implementation of online chat in client online game

--- client/src/app/GameLogic/game/games/game-manager.service.ts
+++ client/src/app/GameLogic/game/games/game-manager.service.ts
@@ -5,6 +5,7 @@ import { GameInfoService } from '@app/GameLogic/game/game-info/game-info.service
 import { OnlineGame } from '@app/GameLogic/game/games/online-game';
 import { TimerService } from '@app/GameLogic/game/timer/timer.service';
 import { MessagesService } from '@app/GameLogic/messages/messages.service';
+import { OnlineChatHandlerService } from '@app/GameLogic/messages/online-chat-handler.service';
 import { BotCreatorService } from '@app/GameLogic/player/bot-creator.service';
 import { Player } from '@app/GameLogic/player/player';
 import { User } from '@app/GameLogic/player/user';
@@ -36,6 +37,7 @@ export class GameManagerService {
         private boardService: BoardService,
         private commandExecuter: CommandExecuterService,
         private gameSocketHandler: GameSocketHandlerService,
+        private onlineChat: OnlineChatHandlerService,
     ) {}
 
     createGame(gameSettings: GameSettings): void {
@@ -71,10 +73,14 @@ export class GameManagerService {
         // TODO: maybe find a way to receive timer time perturn
         const timerPerTurn = Number(gameSettings.timePerTurn);
         this.onlineGame = new OnlineGame(timerPerTurn, userName, this.timer, this.gameSocketHandler, this.boardService);
+
         const opponentName = gameSettings.playerName === userName ? gameSettings.opponentName : gameSettings.playerName;
         const players = this.createOnlinePlayers(userName, opponentName);
         this.allocateOnlinePlayers(players);
+
         this.info.receiveOnlineGame(this.onlineGame);
+
+        this.onlineChat.joinChatRoomWithUser(userAuth.gameToken);
     }
 
     startGame(): void {
