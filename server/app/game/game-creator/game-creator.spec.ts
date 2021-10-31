a6db3ec4c9f829c6954177c6d0e976bb6748c41c
XavierDupuis <xavier0978@hotmail.fr>
2021-10-31 14:39:40 -0400

    gameCreator coverage

---
+++ server/app/game/game-creator/game-creator.spec.ts
@@ -0,0 +1,54 @@
+import { GameCreator } from '@app/game/game-creator/game-creator';
+import { GameStateToken } from '@app/game/game-logic/interface/game-state.interface';
+import { Player } from '@app/game/game-logic/player/player';
+import { PointCalculatorService } from '@app/game/game-logic/point-calculator/point-calculator.service';
+import { getRandomInt } from '@app/game/game-logic/utils';
+import { SystemMessagesService } from '@app/messages-service/system-messages.service';
+import { OnlineGameSettings } from '@app/online-game-init/game-settings-multi.interface';
+import { GameCompiler } from '@app/services/game-compiler.service';
+import { expect } from 'chai';
+import { Subject } from 'rxjs';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
+
+describe('GameCreator', () => {
+    let gameCreator: GameCreator;
+    let onlineGameSettings: OnlineGameSettings;
+    let id: string;
+    let timePerTurn: number;
+    let playerName: string;
+    let opponentName: string;
+    let randomBonus: boolean;
+    let gameToken: string;
+    const pointCalculatorStub: SinonStubbedInstance<PointCalculatorService> = createStubInstance(PointCalculatorService);
+    // let gameCompilerStub: SinonStubbedInstance<GameCompiler> = createStubInstance(GameCompiler);
+    // let systemMessagesServiceStub: SinonStubbedInstance<SystemMessagesService> = createStubInstance(SystemMessagesService);
+    const newGameStateSubject = new Subject<GameStateToken>();
+
+    beforeEach(() => {
+        id = getRandomInt(Number.MAX_SAFE_INTEGER).toString();
+        gameToken = id + 'token';
+        timePerTurn = getRandomInt(Number.MAX_SAFE_INTEGER);
+        playerName = 'p1';
+        opponentName = 'p2';
+        randomBonus = getRandomInt(1) === 0;
+        gameCreator = new GameCreator(pointCalculatorStub, new GameCompiler(), new SystemMessagesService(), newGameStateSubject);
+    });
+
+    it('should create a server game with requested parameters', () => {
+        onlineGameSettings = { id, playerName, opponentName, randomBonus, timePerTurn };
+        const createdGame = gameCreator.createServerGame(onlineGameSettings, gameToken);
+        expect(createdGame.gameToken).to.be.equal(gameToken);
+        expect(createdGame.players).to.be.deep.equal([new Player(playerName), new Player(opponentName)]);
+        expect(createdGame.timePerTurn).to.be.equal(timePerTurn);
+        expect(createdGame.randomBonus).to.be.equal(randomBonus);
+    });
+
+    it('should create a server game with requested parameters and default opponent name', () => {
+        onlineGameSettings = { id, playerName, randomBonus, timePerTurn };
+        const createdGame = gameCreator.createServerGame(onlineGameSettings, gameToken);
+        expect(createdGame.gameToken).to.be.equal(gameToken);
+        expect(createdGame.players).to.be.deep.equal([new Player(playerName), new Player(GameCreator.defaultOpponentName)]);
+        expect(createdGame.timePerTurn).to.be.equal(timePerTurn);
+        expect(createdGame.randomBonus).to.be.equal(randomBonus);
+    });
+});
