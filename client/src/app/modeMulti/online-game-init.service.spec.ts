3682208ae984528d77bab65c6772c1b858fec7f5
Julie-anne <jaeggi.ja@gmail.com>
2021-11-02 12:34:15 -0400

    Test of onlineGameInit

--- client/src/app/modeMulti/online-game-init.service.spec.ts
+++ client/src/app/modeMulti/online-game-init.service.spec.ts
@@ -2,16 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { SocketMock } from '@app/GameLogic/socket-mock';
 import { OnlineGameSettings } from '@app/modeMulti/interface/game-settings-multi.interface';
 import { OnlineGameInitService } from '@app/modeMulti/online-game-init.service';
-// import { io } from 'socket.io-client';
-// import { environment } from 'src/environments/environment';
+import { first, take } from 'rxjs/operators';
+import { Socket } from 'socket.io-client';
 
 fdescribe('OnlineGameInitService', () => {
     let service: OnlineGameInitService;
-    // const tempSocket = io(environment.serverSocketUrl, { path: '/newGame' });
+    let createSocketFunction: () => Socket;
+
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(OnlineGameInitService);
-
+        createSocketFunction = service.connectToSocket;
         service.connectToSocket = jasmine.createSpy().and.returnValue(new SocketMock());
         service.connect();
     });
@@ -27,11 +28,15 @@ fdescribe('OnlineGameInitService', () => {
         }).toThrowError();
     });
 
-    it('createGame should emit createGame if game settings are valid', () => {
-        const spyWaitingForPlayer = spyOn(service, 'waitForSecondPlayer');
+    it('createGame should emit createGame if game settings are valid and receive pendingGameId', () => {
+        const spyWaitingForPlayer = spyOn(service, 'waitForSecondPlayer').and.callThrough();
         const gameSettings = { playerName: 'allo', randomBonus: false, timePerTurn: 65000 };
         service.createGameMulti(gameSettings);
         expect(spyWaitingForPlayer).toHaveBeenCalled();
+        service.pendingGameId$.pipe(take(1)).subscribe((value) => {
+            expect(value[0]).toEqual('aa');
+        });
+        service.socket.peerSideEmit('pendingGameId', 'aa');
     });
 
     it('join pending game should throw error if socket not connected', () => {
@@ -41,12 +46,55 @@ fdescribe('OnlineGameInitService', () => {
         }).toThrowError();
     });
 
-    it('join pending game should emit joinGame', () => {
+    it('join pending game should emit joinGame and receive GameToken', () => {
         spyOnProperty(service.socket, 'connected', 'get').and.returnValue(true);
-        spyOn(service, 'listenErrorMessage');
-        spyOn(service, 'listenForGameToken');
+        spyOn(service, 'listenForGameToken').and.callThrough();
+        spyOn(service, 'disconnectSocket').and.callThrough();
         service.joinPendingGame('abc', 'allo1');
-        expect(service.listenErrorMessage).toHaveBeenCalled();
         expect(service.listenForGameToken).toHaveBeenCalled();
+
+        service.gameToken$.pipe(first()).subscribe((value) => {
+            expect(value[0]).toEqual('aa');
+        });
+        service.socket.peerSideEmit('gameJoined', 'aa');
+        expect(service.disconnectSocket).toHaveBeenCalled();
+    });
+
+    it('listenForPendingGames should return pending games', () => {
+        const pendingGames = [{ id: '1', timePerTurn: 60000, playerName: 'allo1', randomBonus: false }];
+
+        service.pendingGames$.pipe().subscribe((value) => {
+            expect(value).not.toBeUndefined();
+        });
+        service.listenForPendingGames();
+        service.socket.peerSideEmit('pendingGames', pendingGames as OnlineGameSettings[]);
+    });
+
+    it('listenForError should return error message', () => {
+        const errorMessage = 'Cant create game';
+        service.error$.pipe(first()).subscribe((value: string) => {
+            expect(value).toMatch(errorMessage);
+        });
+        service.listenErrorMessage();
+        service.socket.peerSideEmit('error', errorMessage);
+    });
+
+    it('disconnect if connect to server fail', () => {
+        service.connect();
+        service.isDisconnected$.pipe(take(1)).subscribe((value) => {
+            expect(value).toBeTrue();
+        });
+        service.socket.peerSideEmit('connect_error', true);
+    });
+
+    it('should not disconnect if socket not connected', () => {
+        const spyDisconnect = spyOn(service.socket, 'disconnect');
+        service.socket = undefined;
+        service.disconnectSocket();
+        expect(spyDisconnect).not.toHaveBeenCalled();
+    });
+
+    it('connectToSocket should create socket', () => {
+        expect(createSocketFunction().disconnected).toBeTrue();
     });
 });
