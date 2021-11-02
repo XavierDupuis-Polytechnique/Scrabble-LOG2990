669218ea99b92fc4effc32c913234bb815c81569
Julie-anne <jaeggi.ja@gmail.com>
2021-11-02 16:55:37 -0400

    fix lint

--- client/src/app/pages/classic-game/modals/disconnected-from-server/disconnected-from-server.component.spec.ts
+++ client/src/app/pages/classic-game/modals/disconnected-from-server/disconnected-from-server.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DisconnectedFromServerComponent } from './disconnected-from-server.component';
 
-
 describe('DisconnectedFromServerComponent', () => {
     let component: DisconnectedFromServerComponent;
     let fixture: ComponentFixture<DisconnectedFromServerComponent>;
