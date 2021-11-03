77140d37ce9ac82c940e36cef081a4beb1e160c0
XavierDupuis <xavier0978@hotmail.fr>
2021-11-03 11:54:37 -0400

    fix timer tests async

--- server/app/game/game-logic/timer/timer.service.spec.ts
+++ server/app/game/game-logic/timer/timer.service.spec.ts
@@ -2,33 +2,36 @@ import { THOUSAND, THREE, TWO } from '@app/game/game-logic/constants';
 import { TimerController } from '@app/game/game-logic/timer/timer-controller.service';
 import { Timer } from '@app/game/game-logic/timer/timer.service';
 import { expect } from 'chai';
-import { delay } from 'rxjs';
+import { SinonFakeTimers, useFakeTimers } from 'sinon';
 
 describe('TimerService', () => {
     let service: Timer;
+    let clock: SinonFakeTimers;
 
     beforeEach(() => {
         service = new Timer('gameToken', new TimerController());
+        clock = useFakeTimers();
     });
 
     it('should be created', () => {
         expect(service).to.be.instanceOf(Timer);
     });
 
-    it('should time the interval', () => {
+    it('should time the interval', async () => {
         const time = THOUSAND;
         let timerDone = false;
         const time$ = service.start(time);
         time$.subscribe(() => {
             timerDone = true;
         });
-        delay(time / 2);
+        useFakeTimers();
+        clock.tick(time / 2);
         expect(timerDone).to.be.equal(false);
-        delay(time / 2);
+        clock.tick(time / 2);
         expect(timerDone).to.be.equal(true);
     });
 
-    it('should give time left', () => {
+    it('should give time left', async () => {
         const time = 3000;
         let timeLeft: number | undefined;
         service.timeLeft$.subscribe((value: number | undefined) => {
@@ -36,15 +39,15 @@ describe('TimerService', () => {
         });
         service.start(time);
         expect(timeLeft).to.be.equal(time);
-        delay(THOUSAND);
+        clock.tick(THOUSAND);
         expect(timeLeft).to.be.equal(TWO * THOUSAND);
-        // delay(THOUSAND);
-        // expect(timeLeft).to.be.equal(THOUSAND);
-        // delay(THOUSAND);
-        // expect(timeLeft).to.be.equal(0);
+        clock.tick(THOUSAND);
+        expect(timeLeft).to.be.equal(THOUSAND);
+        clock.tick(THOUSAND);
+        expect(timeLeft).to.be.equal(0);
     });
 
-    it('should stop', () => {
+    it('should stop', async () => {
         const time = 4000;
         let timeLeft: number | undefined;
         service.timeLeft$.subscribe((value: number | undefined) => {
@@ -52,10 +55,10 @@ describe('TimerService', () => {
         });
         service.start(time);
         expect(timeLeft).to.be.equal(time);
-        delay(THOUSAND);
+        clock.tick(THOUSAND);
         expect(timeLeft).to.be.equal(THREE * THOUSAND);
         service.stop();
-        delay(THOUSAND);
+        clock.tick(THOUSAND);
         expect(timeLeft).to.be.equal(THREE * THOUSAND);
     });
 });
