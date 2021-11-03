2b52800cd6c53807e9b50e210dbc8f708ad821c8
Julie-anne <jaeggi.ja@gmail.com>
2021-11-03 14:58:34 -0400

    TEST game-compiler 100% coverage

--- server/app/services/game-compiler.service.spec.ts
+++ server/app/services/game-compiler.service.spec.ts
@@ -1,19 +1,100 @@
-// import { BoardService } from '@app/game/game-logic/board/board.service';
-// import { ServerGame } from '@app/game/game-logic/game/server-game';
-// import { PointCalculatorService } from '@app/game/game-logic/point-calculator/point-calculator.service';
-// import { TimerService } from '@app/game/game-logic/timer/timer.service';
-// import { GameCompiler } from '@app/services/game-compiler.service';
-// import { expect } from 'chai';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-unused-expressions */
+import { Board } from '@app/game/game-logic/board/board';
+import { LetterBag } from '@app/game/game-logic/board/letter-bag';
+import { ServerGame } from '@app/game/game-logic/game/server-game';
+import { Player } from '@app/game/game-logic/player/player';
+import { GameCompiler } from '@app/services/game-compiler.service';
+import { createSinonStubInstance, StubbedClass } from '@app/test.util';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
 
-// describe('GameCompiler service', () => {
-//     // Mock game and all of those shit
-//     const timerService = new TimerService();
-//     const boardService = new BoardService();
-//     const pointCalculatorService = new PointCalculatorService(boardService);
-//     const game: ServerGame = new ServerGame(false, 1000, timerService, pointCalculatorService, boardService);
+describe('GameCompiler service', () => {
+    // Mock game and all of those shit
+    const gameCompilerService = new GameCompiler();
+    // const gameToken = 'abc';
+    // const mockGameState$ = new Subject<GameStateToken>();
+    // const players = [
+    //     {
+    //         name: 'Joueur1',
+    //         isActive: true,
+    //         points: 10,
+    //         letterRack: [
+    //             { char: 'a', value: 1 },
+    //             { char: 'b', value: 2 },
+    //             { char: 'c', value: 3 },
+    //         ],
+    //     },
+    //     {
+    //         name: 'Joueur2',
+    //         isActive: false,
+    //         points: 15,
+    //         letterRack: [
+    //             { char: 'e', value: 1 },
+    //             { char: 'b', value: 2 },
+    //             { char: 'm', value: 3 },
+    //         ],
+    //     },
+    // ];
+    const letterBag = new LetterBag();
+    let game: StubbedClass<ServerGame>;
+    // let gameState: GameState;
 
-//     const gameCompiler = new GameCompiler();
-//     it('test', () => {
-//         expect(gameCompiler.compile(game)).to.be('GameState');
-//     });
-// });
+    beforeEach(() => {
+        const board = createSinonStubInstance<Board>(Board);
+        game = createSinonStubInstance<ServerGame>(ServerGame);
+        game.players = [new Player('Joueur1'), new Player('Joueur2')];
+        game.activePlayerIndex = 0;
+        game.board = board;
+        game.letterBag = letterBag;
+        // gameState = {
+        //     players,
+        //     activePlayerIndex: 1,
+        //     grid: boardService.grid,
+        //     lettersRemaining: 50,
+        //     isEndOfGame: false,
+        //     winnerIndex: [],
+        // };
+        // mockGameState$.next({ gameState, gameToken });
+    });
+
+    it('should return an instance of gameState', () => {
+        const gameCompilerSpy = sinon.spy(gameCompilerService, 'compile');
+        const compiledGame = gameCompilerService.compile(game);
+        expect(gameCompilerSpy.returned(compiledGame)).to.be.true;
+    });
+
+    it('should return correct values of game', () => {
+        game.isEndOfGame.returns(false);
+        const compiledGame = gameCompilerService.compile(game);
+        expect(compiledGame.activePlayerIndex).to.equal(game.activePlayerIndex);
+        expect(compiledGame.isEndOfGame).to.equal(game.isEndOfGame());
+        expect(compiledGame.lettersRemaining).to.equal(game.letterBag.lettersLeft);
+    });
+
+    it('should return two winner on endOfGame if getWinner was 2 players', () => {
+        // const gameCompilerSpy = sinon.spy(gameCompilerService, 'compile');
+        game.isEndOfGame.returns(true);
+        game.getWinner.returns(game.players);
+
+        const compiledGame = gameCompilerService.compile(game);
+        expect(compiledGame.winnerIndex.length).to.equal(2);
+    });
+
+    it('should return correct winner on endOfGame', () => {
+        // const gameCompilerSpy = sinon.spy(gameCompilerService, 'compile');
+        game.isEndOfGame.returns(true);
+        game.getWinner.returns([game.players[0]]);
+
+        const compiledGame = gameCompilerService.compile(game);
+        expect(compiledGame.winnerIndex[0]).to.equal(0);
+    });
+    it('should return correct winner on endOfGame', () => {
+        // const gameCompilerSpy = sinon.spy(gameCompilerService, 'compile');
+        game.isEndOfGame.returns(true);
+        game.getWinner.returns([game.players[1]]);
+
+        const compiledGame = gameCompilerService.compile(game);
+        expect(compiledGame.winnerIndex[0]).to.equal(1);
+    });
+});
