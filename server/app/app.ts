b5d470f014d6f57065c61050d61fcdd3bd172ae0
Samuel <laporte.samuel2000@gmail.com>
2021-11-03 14:57:28 -0400

    rename debug controller

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,5 +1,5 @@
 import { HttpException } from '@app/classes/http.exception';
-import { ServerGameController } from '@app/controllers/server-game.controller';
+import { DebugController } from '@app/controllers/debug.controller';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as express from 'express';
@@ -15,7 +15,7 @@ export class Application {
     private readonly internalError: number = StatusCodes.INTERNAL_SERVER_ERROR;
     private readonly swaggerOptions: swaggerJSDoc.Options;
 
-    constructor(private readonly serverGameController: ServerGameController) {
+    constructor(private readonly debugController: DebugController) {
         this.app = express();
 
         this.swaggerOptions = {
@@ -36,7 +36,7 @@ export class Application {
 
     bindRoutes(): void {
         this.app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(this.swaggerOptions)));
-        this.app.use('/api/servergame', this.serverGameController.router);
+        this.app.use('/api/servergame', this.debugController.router);
         this.app.use('/', (req, res) => {
             res.redirect('/api/docs');
         });
