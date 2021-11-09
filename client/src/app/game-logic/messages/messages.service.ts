0440f4045e202177922002157b49ffb18dc65b3b
Julie-anne <jaeggi.ja@gmail.com>
2021-11-09 16:01:32 -0500

    Commande aide

--- client/src/app/game-logic/messages/messages.service.ts
+++ client/src/app/game-logic/messages/messages.service.ts
@@ -75,6 +75,9 @@ export class MessagesService {
         };
         this.addMessageToLog(message);
         const command = this.commandParser.parse(content, forwarder);
+        if (command === CommandType.Help) {
+            return;
+        }
         if (command === CommandType.Exchange) {
             const hiddenLetters = content.split(' ');
             const numberOfLetters = hiddenLetters[1].length;
