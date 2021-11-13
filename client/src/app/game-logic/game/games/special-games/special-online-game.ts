a0cf31e9afce432c962d6a841fe5a3a2327beb3b
XavierDupuis <xavier0978@hotmail.fr>
2021-11-12 21:49:38 -0500

    create SpecialOnlineGame

---
+++ client/src/app/game-logic/game/games/special-games/special-online-game.ts
@@ -0,0 +1,29 @@
+import { OnlineActionCompilerService } from '@app/game-logic/actions/online-actions/online-action-compiler.service';
+import { BoardService } from '@app/game-logic/game/board/board.service';
+import { OnlineGame } from '@app/game-logic/game/games/online-game/online-game';
+import { Objective } from '@app/game-logic/game/objectives/objectives/objective';
+import { TimerService } from '@app/game-logic/game/timer/timer.service';
+import { GameSocketHandlerService } from '@app/socket-handler/game-socket-handler/game-socket-handler.service';
+
+export class SpecialOnlineGame extends OnlineGame {
+    privateObjectives: Map<string, Objective[]>;
+    publicObjectives: Objective[];
+    constructor(
+        public gameToken: string,
+        public timePerTurn: number,
+        public userName: string,
+        timer: TimerService,
+        onlineSocket: GameSocketHandlerService,
+        boardService: BoardService,
+        onlineActionCompiler: OnlineActionCompilerService,
+    ) {
+        super(gameToken, timePerTurn, userName, timer, onlineSocket, boardService, onlineActionCompiler);
+        this.privateObjectives = new Map<string, Objective[]>();
+        this.publicObjectives = [];
+    }
+
+    updateObjectives(privateObjectives: Map<string, Objective[]>, publicObjectives: Objective[]) {
+        this.privateObjectives = privateObjectives;
+        this.publicObjectives = publicObjectives;
+    }
+}
