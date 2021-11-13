1cc817527acf1516b2703815c3e689865ae43ad8
XavierDupuis <xavier0978@hotmail.fr>
2021-11-12 21:27:42 -0500

    create SpecialOfflineGame

---
+++ client/src/app/game-logic/game/games/special-games/special-offline-game.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+import { BoardService } from '@app/game-logic/game/board/board.service';
+import { SpecialOfflineGame } from '@app/game-logic/game/games/special-games/special-offline-game';
+import { ObjectiveManagerService } from '@app/game-logic/game/objectives/objective-manager.service';
+import { TimerService } from '@app/game-logic/game/timer/timer.service';
+import { MessagesService } from '@app/game-logic/messages/messages.service';
+import { PointCalculatorService } from '@app/game-logic/point-calculator/point-calculator.service';
+const TIME_PER_TURN = 10;
+
+describe('SpecialOfflineGame', () => {
+    let game: SpecialOfflineGame;
+    let timerSpy: TimerService;
+    const randomBonus = false;
+    let pointCalculatorSpy: jasmine.SpyObj<PointCalculatorService>;
+    let boardSpy: jasmine.SpyObj<BoardService>;
+    let messageSpy: jasmine.SpyObj<MessagesService>;
+    let objectiveSpy: jasmine.SpyObj<ObjectiveManagerService>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [MessagesService, PointCalculatorService, BoardService, TimerService, PointCalculatorService],
+        });
+        timerSpy = TestBed.inject(TimerService);
+        pointCalculatorSpy = jasmine.createSpyObj('PointCalculatorService', ['endOfGamePointDeduction']);
+        boardSpy = jasmine.createSpyObj('BoardService', ['board']);
+        messageSpy = jasmine.createSpyObj('MessagesService', ['receiveSystemMessage', 'onEndOfGame']);
+        objectiveSpy = jasmine.createSpyObj('ObjectiveManagerService', ['void']);
+        game = new SpecialOfflineGame(randomBonus, TIME_PER_TURN, timerSpy, pointCalculatorSpy, boardSpy, messageSpy, objectiveSpy);
+    });
+
+    it('should be created', () => {
+        expect(game).toBeTruthy();
+    });
+});
---
+++ client/src/app/game-logic/game/games/special-games/special-offline-game.ts
@@ -0,0 +1,43 @@
+import { BoardService } from '@app/game-logic/game/board/board.service';
+import { OfflineGame } from '@app/game-logic/game/games/solo-game/offline-game';
+import { ObjectiveManagerService } from '@app/game-logic/game/objectives/objective-manager.service';
+import { Objective } from '@app/game-logic/game/objectives/objectives/objective';
+import { TimerService } from '@app/game-logic/game/timer/timer.service';
+import { MessagesService } from '@app/game-logic/messages/messages.service';
+import { PointCalculatorService } from '@app/game-logic/point-calculator/point-calculator.service';
+
+export class SpecialOfflineGame extends OfflineGame {
+    privateObjectives: Map<string, Objective[]>;
+    publicObjectives: Objective[];
+    constructor(
+        public randomBonus: boolean,
+        public timePerTurn: number,
+        timer: TimerService,
+        pointCalculator: PointCalculatorService,
+        boardService: BoardService,
+        messagesService: MessagesService,
+        private objectiveManager: ObjectiveManagerService,
+    ) {
+        super(randomBonus, timePerTurn, timer, pointCalculator, boardService, messagesService);
+        this.privateObjectives = new Map<string, Objective[]>();
+        this.publicObjectives = [];
+        this.allocateObjectives();
+    }
+
+    private allocateObjectives() {
+        this.allocatePrivateObjectives();
+        this.allocatePublicObjectives();
+    }
+
+    private allocatePrivateObjectives() {
+        for (const player of this.players) {
+            const playerPrivateObjectives = this.objectiveManager.chooseObjectives(ObjectiveManagerService.privateObjectiveCount);
+            this.privateObjectives.set(player.name, playerPrivateObjectives);
+        }
+    }
+
+    private allocatePublicObjectives() {
+        const publicObjectives = this.objectiveManager.chooseObjectives(ObjectiveManagerService.publicObjectiveCount);
+        this.publicObjectives = publicObjectives;
+    }
+}
