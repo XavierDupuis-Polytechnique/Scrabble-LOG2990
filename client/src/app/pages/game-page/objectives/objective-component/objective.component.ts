d65b7715221027a22dcc70d2a4cadbadf3efe635
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-11-14 23:34:57 -0500

    feat(objective-ui): objective status

--- client/src/app/pages/game-page/objectives/objective-component/objective.component.ts
+++ client/src/app/pages/game-page/objectives/objective-component/objective.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { GameInfoService } from '@app/game-logic/game/game-info/game-info.service';
 import { Objective } from '@app/game-logic/game/objectives/objectives/objective';
+import { ObjectiveStatus } from '@app/pages/game-page/objectives/objectives-status.enum';
 
 @Component({
     selector: 'app-objective',
@@ -8,6 +10,19 @@ import { Objective } from '@app/game-logic/game/objectives/objectives/objective'
 })
 export class ObjectiveComponent {
     @Input() objective: Objective;
+
+    constructor(private info: GameInfoService) {}
+
+    get status(): ObjectiveStatus {
+        if (this.objective.owner === undefined) {
+            return ObjectiveStatus.NotClaimed;
+        }
+
+        if (this.objective.owner === this.info.user.name) {
+            return ObjectiveStatus.Won;
+        }
+        return ObjectiveStatus.Lost;
+    }
 
     get name(): string {
         return this.objective.name;
