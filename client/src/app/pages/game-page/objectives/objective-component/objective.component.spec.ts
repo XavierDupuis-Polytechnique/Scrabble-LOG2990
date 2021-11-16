5f5c105d5e4b28c6abd3365f79679f123898ba3c
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-11-16 15:16:40 -0500

    fix(test): fix objective component test

--- client/src/app/pages/game-page/objectives/objective-component/objective.component.spec.ts
+++ client/src/app/pages/game-page/objectives/objective-component/objective.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GameInfoService } from '@app/game-logic/game/game-info/game-info.service';
+import { FourCorners } from '@app/game-logic/game/objectives/objectives/four-corners';
+import { User } from '@app/game-logic/player/user';
 
 import { ObjectiveComponent } from './objective.component';
 
 describe('ObjectiveComponent', () => {
     let component: ObjectiveComponent;
     let fixture: ComponentFixture<ObjectiveComponent>;
-
     beforeEach(async () => {
+        const mockInfoService = jasmine.createSpyObj('GameInfoService', ['getObjective']);
+        mockInfoService.user = new User('test');
         await TestBed.configureTestingModule({
+            providers: [{ provide: GameInfoService, useValue: mockInfoService }],
             declarations: [ObjectiveComponent],
         }).compileComponents();
     });
@@ -15,6 +20,8 @@ describe('ObjectiveComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(ObjectiveComponent);
         component = fixture.componentInstance;
+        // TODO: create a mock objective
+        component.objective = new FourCorners();
         fixture.detectChanges();
     });
 
