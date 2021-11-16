541a45cb23fe9078103b8087800ce1afe0ad46ec
XavierDupuis <xavier0978@hotmail.fr>
2021-11-16 18:22:10 -0500

    ObjectiveNotifier

---
+++ client/src/app/game-logic/game/objectives/objective-notifier/objective-notifier.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ObjectiveNotifierService } from './objective-notifier.service';
+
+describe('ObjectiveNotifierService', () => {
+  let service: ObjectiveNotifierService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ObjectiveNotifierService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
