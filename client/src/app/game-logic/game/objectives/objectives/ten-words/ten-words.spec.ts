bf92a4c26c7dcd72acafa694abb797dd147a9ea7
Olivier Saint-Cyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-11-17 00:53:56 -0500

    feat(ten-word): ten word objective implementation

--- client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.spec.ts
+++ client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { Action } from '@app/game-logic/actions/action';
+import { PlaceLetter } from '@app/game-logic/actions/place-letter';
 import { ObjectiveNotifierService } from '@app/game-logic/game/objectives/objective-notifier/objective-notifier.service';
-import { TenWords } from '@app/game-logic/game/objectives/objectives/ten-words/ten-words';
+import { ObjectiveUpdateParams } from '@app/game-logic/game/objectives/objectives/objective-update-params.interface';
+import { N_WORD_TO_PLACE, TenWords } from '@app/game-logic/game/objectives/objectives/ten-words/ten-words';
 
-describe('FifteenWords', () => {
+describe('TenWords', () => {
     let objective: TenWords;
-
+    let action: PlaceLetter;
     beforeEach(() => {
         TestBed.configureTestingModule({});
         objective = new TenWords(TestBed.inject(ObjectiveNotifierService));
+        action = jasmine.createSpyObj(Action, ['execute']);
     });
 
     it('should be created', () => {
         expect(objective).toBeTruthy();
+    });
+
+    it('should complete', () => {
+        const params: ObjectiveUpdateParams = {
+            previousGrid: [],
+            currentGrid: [],
+            lettersToPlace: [],
+            formedWords: [],
+        };
+        for (let i = 0; i < N_WORD_TO_PLACE; i++) {
+            objective.updateProgression(action, params);
+        }
+        expect(objective.progression).toBe(1);
     });
 });
--- client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.ts
+++ client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.ts
@@ -2,11 +2,15 @@ import { Action } from '@app/game-logic/actions/action';
 import { Objective } from '@app/game-logic/game/objectives/objectives/objective';
 import { ObjectiveUpdateParams } from '@app/game-logic/game/objectives/objectives/objective-update-params.interface';
 
+export const N_WORD_TO_PLACE = 10;
 export class TenWords extends Objective {
     name = '10 mots';
     description = 'Placer au moins 10 mots dans une partie.';
+    private wordCount = 0;
 
-    updateProgression(action: Action, params:  ObjectiveUpdateParams): void {
-        throw new Error('Method not implemented.');
+    // eslint-disable-next-line no-unused-vars
+    updateProgression(action: Action, params: ObjectiveUpdateParams): void {
+        this.wordCount++;
+        this.progression = this.wordCount / N_WORD_TO_PLACE;
     }
 }
