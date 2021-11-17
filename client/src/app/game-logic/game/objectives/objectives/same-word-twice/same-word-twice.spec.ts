fb4589c8a7889b757b087e3b56b6ab4d3a2e9425
Olivier Saint-Cyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-11-17 00:53:33 -0500

    feat(same-word-twice): implementation of two-word-twice

--- client/src/app/game-logic/game/objectives/objectives/same-word-twice/same-word-twice.spec.ts
+++ client/src/app/game-logic/game/objectives/objectives/same-word-twice/same-word-twice.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
+import { Action } from '@app/game-logic/actions/action';
+import { Tile } from '@app/game-logic/game/board/tile';
 import { ObjectiveNotifierService } from '@app/game-logic/game/objectives/objective-notifier/objective-notifier.service';
+import { ObjectiveUpdateParams } from '@app/game-logic/game/objectives/objectives/objective-update-params.interface';
 import { SameWordTwice } from '@app/game-logic/game/objectives/objectives/same-word-twice/same-word-twice';
 
 describe('SameWordTwice', () => {
     let objective: SameWordTwice;
-
+    let action: Action;
     beforeEach(() => {
         TestBed.configureTestingModule({});
         objective = new SameWordTwice(TestBed.inject(ObjectiveNotifierService));
+        action = jasmine.createSpyObj(Action, ['execute']);
     });
 
     it('should be created', () => {
         expect(objective).toBeTruthy();
+    });
+
+    it('should be completed', () => {
+        const tile1 = new Tile();
+        tile1.letterObject = { char: 'A', value: 0 };
+        const tile2 = new Tile();
+        tile2.letterObject = { char: 'B', value: 0 };
+        const params: ObjectiveUpdateParams = {
+            currentGrid: [],
+            previousGrid: [],
+            lettersToPlace: [],
+            formedWords: [
+                [tile1, tile2],
+                [tile1, tile2],
+            ],
+        };
+        objective.updateProgression(action, params);
+        expect(objective.progression).toBe(1);
+    });
+
+    it('should not be completed', () => {
+        const tile1 = new Tile();
+        tile1.letterObject = { char: 'A', value: 0 };
+        const tile2 = new Tile();
+        tile2.letterObject = { char: 'B', value: 0 };
+        const params: ObjectiveUpdateParams = {
+            currentGrid: [],
+            previousGrid: [],
+            lettersToPlace: [],
+            formedWords: [
+                [tile1, tile2],
+                [tile1, tile1],
+            ],
+        };
+        objective.updateProgression(action, params);
+        expect(objective.progression).toBe(0);
     });
 });
--- client/src/app/game-logic/game/objectives/objectives/same-word-twice/same-word-twice.ts
+++ client/src/app/game-logic/game/objectives/objectives/same-word-twice/same-word-twice.ts
@@ -1,12 +1,21 @@
 import { Action } from '@app/game-logic/actions/action';
 import { Objective } from '@app/game-logic/game/objectives/objectives/objective';
 import { ObjectiveUpdateParams } from '@app/game-logic/game/objectives/objectives/objective-update-params.interface';
+import { stringifyWord } from '@app/game-logic/utils';
 
 export class SameWordTwice extends Objective {
     name = "1 c'est bien mais 2 c'est mieux";
     description = 'Former deux fois le même mot dans un même placement';
 
-    updateProgression(action: Action, params:  ObjectiveUpdateParams): void {
-        throw new Error('Method not implemented.');
+    updateProgression(action: Action, params: ObjectiveUpdateParams): void {
+        const formedWords: string[] = params.formedWords.map((word) => stringifyWord(word));
+        const wordSet = new Set<string>();
+        for (const word of formedWords) {
+            if (wordSet.has(word)) {
+                this.progression = 1;
+                break;
+            }
+            wordSet.add(word);
+        }
     }
 }
