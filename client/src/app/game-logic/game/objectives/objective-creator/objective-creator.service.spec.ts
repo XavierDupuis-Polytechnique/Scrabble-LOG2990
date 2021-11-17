fef6dcec883db73d58abc1200ff7b67f5514f8e3
XavierDupuis <xavier0978@hotmail.fr>
2021-11-17 14:30:23 -0500

    proper name for tests

--- client/src/app/game-logic/game/objectives/objective-creator/objective-creator.service.spec.ts
+++ client/src/app/game-logic/game/objectives/objective-creator/objective-creator.service.spec.ts
@@ -4,37 +4,37 @@ import { ObjectiveNotifierService } from '@app/game-logic/game/objectives/object
 import { ObjectiveCreator } from './objective-creator.service';
 
 fdescribe('ObjectiveManager', () => {
-    let objectiveCreator: ObjectiveCreator;
+    let service: ObjectiveCreator;
     let objectiveNotifierSpy: jasmine.SpyObj<ObjectiveNotifierService>;
 
     beforeEach(() => {
         objectiveNotifierSpy = jasmine.createSpyObj(ObjectiveNotifierService, ['sendObjectiveNotification']);
         TestBed.configureTestingModule({ providers: [{ provide: ObjectiveNotifierService, useValue: objectiveNotifierSpy }] });
-        objectiveCreator = new ObjectiveCreator(TestBed.inject(ObjectiveNotifierService));
+        service = new ObjectiveCreator(TestBed.inject(ObjectiveNotifierService));
     });
 
     it('should be created', () => {
-        expect(objectiveCreator).toBeTruthy();
+        expect(service).toBeTruthy();
     });
 
     it('should to create N unique objective', () => {
-        const createdObjectives = objectiveCreator.chooseObjectives(TOTAL_OBJECTIVE_COUNT);
+        const createdObjectives = service.chooseObjectives(TOTAL_OBJECTIVE_COUNT);
         const objectiveNamesSet = new Set<string>(createdObjectives.map((objective) => objective.name));
         expect(createdObjectives.length).toBe(TOTAL_OBJECTIVE_COUNT);
         expect(objectiveNamesSet.size).toBe(TOTAL_OBJECTIVE_COUNT);
-        expect(objectiveCreator.availableObjectivesIndex.length).toBe(0);
+        expect(service.availableObjectivesIndex.length).toBe(0);
     });
 
     it('should to create N unique objective and then M unique objectives with no overlap', () => {
-        const createdObjectives1 = objectiveCreator.chooseObjectives(PUBLIC_OBJECTIVE_COUNT);
+        const createdObjectives1 = service.chooseObjectives(PUBLIC_OBJECTIVE_COUNT);
         const objectiveNamesSet1 = new Set<string>(createdObjectives1.map((objective) => objective.name));
         expect(objectiveNamesSet1.size).toBe(PUBLIC_OBJECTIVE_COUNT);
 
-        const createdObjectives2 = objectiveCreator.chooseObjectives(PUBLIC_OBJECTIVE_COUNT);
+        const createdObjectives2 = service.chooseObjectives(PUBLIC_OBJECTIVE_COUNT);
         const objectiveNamesSet2 = new Set<string>(createdObjectives2.map((objective) => objective.name));
         expect(objectiveNamesSet2.size).toBe(PUBLIC_OBJECTIVE_COUNT);
 
-        expect(objectiveCreator.availableObjectivesIndex.length).toBe(TOTAL_OBJECTIVE_COUNT - 2 * PUBLIC_OBJECTIVE_COUNT);
+        expect(service.availableObjectivesIndex.length).toBe(TOTAL_OBJECTIVE_COUNT - 2 * PUBLIC_OBJECTIVE_COUNT);
         for (const objective of objectiveNamesSet2) {
             expect(objectiveNamesSet1.has(objective)).toBeFalsy();
         }
@@ -42,7 +42,7 @@ fdescribe('ObjectiveManager', () => {
 
     it('should throw error when requesting more than TOTAL_OBJECTIVE_COUNT unique objectives', () => {
         expect(() => {
-            objectiveCreator.chooseObjectives(TOTAL_OBJECTIVE_COUNT + 1);
+            service.chooseObjectives(TOTAL_OBJECTIVE_COUNT + 1);
         }).toThrow();
     });
 });
