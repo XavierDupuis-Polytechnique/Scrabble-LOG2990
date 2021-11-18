0d89e6956f54c2752d57742e0372d87a19df46d9
XavierDupuis <xavier0978@hotmail.fr>
2021-11-18 10:00:38 -0500

    fix TenWords impl

--- client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.ts
+++ client/src/app/game-logic/game/objectives/objectives/ten-words/ten-words.ts
@@ -5,11 +5,16 @@ export class TenWords extends Objective {
     name = '10 mots';
     description = 'Placer au moins 10 mots dans une partie.';
     points = TEN_WORDS_POINTS;
-    private wordCount = 0;
+    private wordCounts = new Map<string, number>();
 
     protected updateProgression(action: Action): void {
-        this.wordCount++;
-        const newProgression = this.wordCount / TEN_WORDS_NUMBER_OF_WORDS_TO_PLACE;
+        let wordCount = this.wordCounts.get(action.player.name);
+        if (wordCount === undefined) {
+            wordCount = 0;
+        }
+        wordCount++;
+        this.wordCounts.set(action.player.name, wordCount);
+        const newProgression = wordCount / TEN_WORDS_NUMBER_OF_WORDS_TO_PLACE;
         this.setPlayerProgression(action.player.name, newProgression);
     }
 }
