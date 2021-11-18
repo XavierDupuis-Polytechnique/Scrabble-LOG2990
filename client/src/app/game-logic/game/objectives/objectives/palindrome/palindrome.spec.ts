411a72665b833fa84f25fde211cc8903a50c9728
XavierDupuis <xavier0978@hotmail.fr>
2021-11-17 20:22:28 -0500

    full palindrome tests

--- client/src/app/game-logic/game/objectives/objectives/palindrome/palindrome.spec.ts
+++ client/src/app/game-logic/game/objectives/objectives/palindrome/palindrome.spec.ts
@@ -1,11 +1,11 @@
 /* eslint-disable max-classes-per-file */
 import { TestBed } from '@angular/core/testing';
 import { Action } from '@app/game-logic/actions/action';
-import { Tile } from '@app/game-logic/game/board/tile';
 import { ObjectiveNotifierService } from '@app/game-logic/game/objectives/objective-notifier/objective-notifier.service';
 import { ObjectiveUpdateParams } from '@app/game-logic/game/objectives/objectives/objective-update-params.interface';
 import { Palindrome } from '@app/game-logic/game/objectives/objectives/palindrome/palindrome';
 import { Player } from '@app/game-logic/player/player';
+import { wordifyString } from '@app/game-logic/utils';
 
 class MockAction extends Action {
     protected perform(): void {
@@ -24,6 +24,7 @@ describe('Palindrome', () => {
     let player: Player;
     let action: Action;
     let objectiveNotifierSpy: jasmine.SpyObj<ObjectiveNotifierService>;
+    let params: ObjectiveUpdateParams;
 
     beforeEach(() => {
         objectiveNotifierSpy = jasmine.createSpyObj(ObjectiveNotifierService, ['sendObjectiveNotification']);
@@ -31,51 +32,60 @@ describe('Palindrome', () => {
         objective = new Palindrome(TestBed.inject(ObjectiveNotifierService));
         player = new MockPlayer();
         action = new MockAction(player);
+        params = {
+            previousGrid: [],
+            currentGrid: [],
+            lettersToPlace: [],
+            formedWords: [],
+        };
     });
 
     it('should be created', () => {
         expect(objective).toBeTruthy();
     });
 
-    it('should complete the objective', () => {
-        const tile1 = new Tile();
-        tile1.letterObject = { char: 'A', value: 0 };
-        const tile2 = new Tile();
-        tile2.letterObject = { char: 'B', value: 0 };
-        const tile3 = new Tile();
-        tile3.letterObject = { char: 'C', value: 0 };
-        const tile4 = new Tile();
-        tile4.letterObject = { char: 'B', value: 0 };
-        const tile5 = new Tile();
-        tile5.letterObject = { char: 'A', value: 0 };
-        const params: ObjectiveUpdateParams = {
-            previousGrid: [],
-            currentGrid: [],
-            lettersToPlace: [],
-            formedWords: [[tile1, tile2, tile3, tile4, tile5]],
-        };
+    it('should complete the objective with ABCBA', () => {
+        params.formedWords.push(wordifyString('ABCBA'));
         objective.update(action, params);
         expect(objective.getPlayerProgression(action.player.name)).toBe(1);
+        expect(objective.owner).toBe(player.name);
+        expect(player.points).toBe(objective.points);
+        expect(objectiveNotifierSpy.sendObjectiveNotification).toHaveBeenCalledOnceWith(objective);
     });
 
-    it('should not complete the objective', () => {
-        const tile1 = new Tile();
-        tile1.letterObject = { char: 'A', value: 0 };
-        const tile2 = new Tile();
-        tile2.letterObject = { char: 'B', value: 0 };
-        const tile3 = new Tile();
-        tile3.letterObject = { char: 'C', value: 0 };
-        const tile4 = new Tile();
-        tile4.letterObject = { char: 'B', value: 0 };
-        const tile5 = new Tile();
-        tile5.letterObject = { char: 'W', value: 0 };
-        const params: ObjectiveUpdateParams = {
-            previousGrid: [],
-            currentGrid: [],
-            lettersToPlace: [],
-            formedWords: [[tile1, tile2, tile3, tile4, tile5]],
-        };
+    it('should complete the objective with ABCCBA', () => {
+        params.formedWords.push(wordifyString('ABCCBA'));
+        objective.update(action, params);
+        expect(objective.getPlayerProgression(action.player.name)).toBe(1);
+        expect(objective.owner).toBe(player.name);
+        expect(player.points).toBe(objective.points);
+        expect(objectiveNotifierSpy.sendObjectiveNotification).toHaveBeenCalledOnceWith(objective);
+    });
+
+    it('should not complete the objective with AABCCBA', () => {
+        params.formedWords.push(wordifyString('AABCCBA'));
         objective.update(action, params);
         expect(objective.getPlayerProgression(action.player.name)).toBe(0);
+        expect(objective.owner).toBeUndefined();
+        expect(player.points).toBe(0);
+        expect(objectiveNotifierSpy.sendObjectiveNotification).not.toHaveBeenCalled();
+    });
+
+    it('should not complete the objective with ABCBW', () => {
+        params.formedWords.push(wordifyString('ABCBW'));
+        objective.update(action, params);
+        expect(objective.getPlayerProgression(action.player.name)).toBe(0);
+        expect(objective.owner).toBeUndefined();
+        expect(player.points).toBe(0);
+        expect(objectiveNotifierSpy.sendObjectiveNotification).not.toHaveBeenCalled();
+    });
+
+    it('should not complete the objective with ABCCBW', () => {
+        params.formedWords.push(wordifyString('ABCCBW'));
+        objective.update(action, params);
+        expect(objective.getPlayerProgression(action.player.name)).toBe(0);
+        expect(objective.owner).toBeUndefined();
+        expect(player.points).toBe(0);
+        expect(objectiveNotifierSpy.sendObjectiveNotification).not.toHaveBeenCalled();
     });
 });
