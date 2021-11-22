ea58596d758e7a66cad03bc21bf8dea507178cb2
XavierDupuis <xavier0978@hotmail.fr>
2021-11-22 10:57:13 -0500

    test(online-objective) : coverage

--- client/src/app/game-logic/game/games/game-manager/game-manager.service.ts
+++ client/src/app/game-logic/game/games/game-manager/game-manager.service.ts
@@ -101,13 +101,15 @@ export class GameManagerService {
         if (this.game) {
             this.stopGame();
         }
+
         if (!gameSettings.opponentName) {
             throw Error('No opponent name was entered');
         }
+
         if (!gameSettings.playerName) {
             throw Error('player name not entered');
         }
-        console.log(userAuth, gameSettings);
+
         const userName = userAuth.playerName;
         const timerPerTurn = Number(gameSettings.timePerTurn);
         if (gameSettings.gameMode === GameMode.Classic) {
