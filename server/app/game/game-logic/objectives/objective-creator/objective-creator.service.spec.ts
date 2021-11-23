b9e338ac365bcaec60bdaadfc671e6a0adcdbe00
XavierDupuis <xavier0978@hotmail.fr>
2021-11-23 09:53:34 -0500

    fix(objective-crreator) : updated tests

--- server/app/game/game-logic/objectives/objective-creator/objective-creator.service.spec.ts
+++ server/app/game/game-logic/objectives/objective-creator/objective-creator.service.spec.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_OBJECTIVE_COUNT, TOTAL_OBJECTIVE_COUNT } from '@app/constants';
+import { PRIVATE_OBJECTIVE_COUNT, PUBLIC_OBJECTIVE_COUNT, TOTAL_OBJECTIVE_COUNT } from '@app/constants';
 import { ObjectiveCreator } from '@app/game/game-logic/objectives/objective-creator/objective-creator.service';
 import { ObjectiveNotifierService } from '@app/game/game-logic/objectives/objective-notifier/objective-notifier.service';
 import { createSinonStubInstance } from '@app/test.util';
@@ -18,31 +18,43 @@ describe('ObjectiveCreator', () => {
     });
 
     it('should to create N unique objective', () => {
-        const createdObjectives = service.chooseObjectives(gameToken, TOTAL_OBJECTIVE_COUNT);
-        const objectiveNamesSet = new Set<string>(createdObjectives.map((objective) => objective.name));
-        expect(createdObjectives.length).to.be.equal(TOTAL_OBJECTIVE_COUNT);
-        expect(objectiveNamesSet.size).to.be.equal(TOTAL_OBJECTIVE_COUNT);
-        expect(service.availableObjectivesIndex.length).to.be.equal(0);
-    });
-
-    it('should to create N unique objective and then M unique objectives with no overlap', () => {
-        const createdObjectives1 = service.chooseObjectives(gameToken, PUBLIC_OBJECTIVE_COUNT);
-        const objectiveNamesSet1 = new Set<string>(createdObjectives1.map((objective) => objective.name));
-        expect(objectiveNamesSet1.size).to.be.equal(PUBLIC_OBJECTIVE_COUNT);
-
-        const createdObjectives2 = service.chooseObjectives(gameToken, PUBLIC_OBJECTIVE_COUNT);
-        const objectiveNamesSet2 = new Set<string>(createdObjectives2.map((objective) => objective.name));
-        expect(objectiveNamesSet2.size).to.be.equal(PUBLIC_OBJECTIVE_COUNT);
-
-        expect(service.availableObjectivesIndex.length).to.be.equal(TOTAL_OBJECTIVE_COUNT - 2 * PUBLIC_OBJECTIVE_COUNT);
-        for (const objective of objectiveNamesSet2) {
-            expect(objectiveNamesSet1.has(objective)).to.be.equal(false);
+        const createdObjectives = service.chooseObjectives(gameToken);
+        const publicObjectives = createdObjectives.publicObjectives;
+        const objectivesNamesSet = new Set<string>(publicObjectives.map((objective) => objective.name));
+        for (const privateObjectiveList of createdObjectives.privateObjectives) {
+            for (const privateObjective of privateObjectiveList) {
+                objectivesNamesSet.add(privateObjective.name);
+            }
         }
+        expect(objectivesNamesSet.size).to.be.equal(PRIVATE_OBJECTIVE_COUNT * 2 + PUBLIC_OBJECTIVE_COUNT);
     });
 
-    it('should throw error when requesting more than TOTAL_OBJECTIVE_COUNT unique objectives', () => {
+    it('should to create N unique objective and then N unique objectives with no overlap', () => {
+        const createdObjectives1 = service.chooseObjectives(gameToken);
+        const publicObjectives1 = createdObjectives1.publicObjectives;
+        const objectivesNamesSet1 = new Set<string>(publicObjectives1.map((objective) => objective.name));
+        for (const privateObjectiveList of createdObjectives1.privateObjectives) {
+            for (const privateObjective of privateObjectiveList) {
+                objectivesNamesSet1.add(privateObjective.name);
+            }
+        }
+        expect(objectivesNamesSet1.size).to.be.equal(PRIVATE_OBJECTIVE_COUNT * 2 + PUBLIC_OBJECTIVE_COUNT);
+
+        const createdObjectives2 = service.chooseObjectives(gameToken);
+        const publicObjectives2 = createdObjectives2.publicObjectives;
+        const objectivesNamesSet2 = new Set<string>(publicObjectives2.map((objective) => objective.name));
+        for (const privateObjectiveList of createdObjectives2.privateObjectives) {
+            for (const privateObjective of privateObjectiveList) {
+                objectivesNamesSet2.add(privateObjective.name);
+            }
+        }
+        expect(objectivesNamesSet2.size).to.be.equal(PRIVATE_OBJECTIVE_COUNT * 2 + PUBLIC_OBJECTIVE_COUNT);
+    });
+
+    it('should throw error when requesting an unknown objective index', () => {
         expect(() => {
-            service.chooseObjectives(gameToken, TOTAL_OBJECTIVE_COUNT + 1);
+            // eslint-disable-next-line dot-notation
+            service['createObjective'](gameToken, TOTAL_OBJECTIVE_COUNT + 1);
         }).to.throw();
     });
 });
