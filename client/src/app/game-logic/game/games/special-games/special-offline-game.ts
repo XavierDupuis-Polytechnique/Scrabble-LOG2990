210651e8ec4f97719e4eeadc4d6679438193e31d
dasadou <65833938+dasadou@users.noreply.github.com>
2021-11-25 17:36:05 -0500

    Refactor de qui set la progression initial a 0

--- client/src/app/game-logic/game/games/special-games/special-offline-game.ts
+++ client/src/app/game-logic/game/games/special-games/special-offline-game.ts
@@ -30,12 +30,19 @@ export class SpecialOfflineGame extends OfflineGame implements SpecialGame {
     allocateObjectives() {
         const generatedObjectives = this.objectiveCreator.chooseObjectives();
         this.publicObjectives = generatedObjectives.publicObjectives;
-
+        for (const objective of this.publicObjectives) {
+            this.players.forEach((player) => {
+                objective.progressions.set(player.name, 0);
+            });
+        }
         this.privateObjectives = new Map<string, Objective[]>();
         for (let index = 0; index < this.players.length; index++) {
             const playerName = this.players[index].name;
             const privateObjectives = generatedObjectives.privateObjectives[index];
             this.privateObjectives.set(playerName, privateObjectives);
+            for (const objective of privateObjectives) {
+                objective.progressions.set(playerName, 0);
+            }
         }
     }
 
--- server/app/game/game-logic/game/special-server-game.ts
+++ server/app/game/game-logic/game/special-server-game.ts
@@ -45,12 +45,19 @@ export class SpecialServerGame extends ServerGame {
     allocateObjectives() {
         const generatedObjectives = this.objectiveCreator.chooseObjectives(this.gameToken);
         this.publicObjectives = generatedObjectives.publicObjectives;
-
+        for (const objective of this.publicObjectives) {
+            this.players.forEach((player) => {
+                objective.progressions.set(player.name, 0);
+            });
+        }
         this.privateObjectives = new Map<string, Objective[]>();
         for (let index = 0; index < this.players.length; index++) {
             const playerName = this.players[index].name;
             const privateObjectives = generatedObjectives.privateObjectives[index];
             this.privateObjectives.set(playerName, privateObjectives);
+            for (const objective of privateObjectives) {
+                objective.progressions.set(playerName, 0);
+            }
         }
     }
 
