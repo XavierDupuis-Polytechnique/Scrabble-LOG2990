fb720f1bd1adf4e68ddbcd4a21f3cc01d02e34c7
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2021-11-28 15:36:34 -0500

    ui(abandon-dialog-component): Changing the component style

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderBarComponent } from '@app/components/header-bar/header-bar.component';
+import { AbandonDialogComponent } from '@app/components/modals/abandon-button/abandon-dialog.component';
 import { ConvertToSoloFormComponent } from '@app/components/modals/convert-to-solo-form/convert-to-solo-form.component';
 import { DisconnectedFromServerComponent } from '@app/components/modals/disconnected-from-server/disconnected-from-server.component';
 import { ErrorDialogComponent } from '@app/components/modals/error-dialog/error-dialog.component';
@@ -68,6 +69,7 @@ import { ObjectivesListComponent } from './pages/game-page/objectives/objectives
         MouseRollDirective,
         ObjectivesListComponent,
         ObjectiveComponent,
+        AbandonDialogComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
     providers: [
