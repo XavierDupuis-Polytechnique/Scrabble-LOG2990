04297d70145d48b5a6e416374de2947309d8b6b1
XavierDupuis <xavier0978@hotmail.fr>
2021-11-30 10:44:21 -0500

    wip test

--- server/app/game/game-logic/objectives/objectives/objective-converter/online-objective-converter.spec.ts
+++ server/app/game/game-logic/objectives/objectives/objective-converter/online-objective-converter.spec.ts
@@ -1,33 +1,57 @@
-/* eslint-disable dot-notation */
-import { ObjectiveCreator } from '@app/game/game-logic/objectives/objective-creator/objective-creator.service';
 import { ObjectiveType } from '@app/game/game-logic/objectives/objective-creator/objective-type';
+import { ObjectiveNotifierService } from '@app/game/game-logic/objectives/objective-notifier/objective-notifier.service';
+import { FourCorners } from '@app/game/game-logic/objectives/objectives/four-corners/four-corners';
+import { HalfAlphabet } from '@app/game/game-logic/objectives/objectives/half-alphabet/half-alphabet';
+import { NineLettersWord } from '@app/game/game-logic/objectives/objectives/nine-letters-word/nine-letters-word';
 import { Objective } from '@app/game/game-logic/objectives/objectives/objective';
 import { OnlineObjectiveConverter } from '@app/game/game-logic/objectives/objectives/objective-converter/online-objective-converter';
 import { TransitionObjectives } from '@app/game/game-logic/objectives/objectives/objective-converter/transition-objectives';
-import { createSinonStubInstance } from '@app/test.util';
+import { Palindrome } from '@app/game/game-logic/objectives/objectives/palindrome/palindrome';
+import { SameWordTwice } from '@app/game/game-logic/objectives/objectives/same-word-twice/same-word-twice';
+import { TenWords } from '@app/game/game-logic/objectives/objectives/ten-words/ten-words';
+import { ThreeSameLetters } from '@app/game/game-logic/objectives/objectives/three-same-letters/three-same-letters';
+import { TripleBonus } from '@app/game/game-logic/objectives/objectives/triple-bonus/triple-bonus';
+import { createSinonStubInstance, StubbedClass } from '@app/test.util';
 import { expect } from 'chai';
 
 describe('OnlineObjectiveConverter', () => {
-    let onlineObjectiveConverter: OnlineObjectiveConverter;
-    let stubObjectiveCreator: ObjectiveCreator;
+    const onlineObjectiveConverter = new OnlineObjectiveConverter();
+    let stubObjectiveNotifier: StubbedClass<ObjectiveNotifierService>;
+    const gameToken = 'gameToken';
+    let totalNumberOfObjectives: number;
+    let objectives: Objective[];
 
-    before(() => {
-        stubObjectiveCreator = createSinonStubInstance<ObjectiveCreator>(ObjectiveCreator);
+    beforeEach(() => {
+        stubObjectiveNotifier = createSinonStubInstance<ObjectiveNotifierService>(ObjectiveNotifierService);
+        totalNumberOfObjectives = Object.keys(ObjectiveType).length / 2;
+        objectives = [
+            new FourCorners(gameToken, stubObjectiveNotifier),
+            new TripleBonus(gameToken, stubObjectiveNotifier),
+            new Palindrome(gameToken, stubObjectiveNotifier),
+            new TenWords(gameToken, stubObjectiveNotifier),
+            new NineLettersWord(gameToken, stubObjectiveNotifier),
+            new HalfAlphabet(gameToken, stubObjectiveNotifier),
+            new SameWordTwice(gameToken, stubObjectiveNotifier),
+            new ThreeSameLetters(gameToken, stubObjectiveNotifier),
+        ];
+    });
+
+    it('should create', () => {
+        expect(onlineObjectiveConverter).to.be.instanceOf(OnlineObjectiveConverter);
     });
 
     it('should create adequate transition objectives', () => {
-        const publicObjective: Objective[] = [];
+        const publicObjectives: Objective[] = [];
+        const privateObjectives = new Map<string, Objective[]>();
         const expectedTransitionObjectives: TransitionObjectives[] = [];
-        const publicObjectiveCount = Object.keys(ObjectiveType).length / 2;
-        let privateObjectiveCount = 0;
 
-        for (let i = 0; i < publicObjectiveCount; i++) {
-            const objective = stubObjectiveCreator['createObjective']('1', i);
-            if (i < publicObjectiveCount) {
-                publicObjective.push(objective);
+        for (let i = 0; i < totalNumberOfObjectives; i++) {
+            const objective = objectives[i];
+            if (i < totalNumberOfObjectives - 1) {
+                publicObjectives.push(objective);
+            } else {
+                privateObjectives.set('p1', [objective]);
             }
-
-            privateObjectiveCount = i + 1;
             const expectedTransitionObjective: TransitionObjectives = {
                 objectiveType: i,
                 name: objective.name,
@@ -38,14 +62,7 @@ describe('OnlineObjectiveConverter', () => {
             };
             expectedTransitionObjectives.push(expectedTransitionObjective);
         }
-
-        const privateObjectives = new Map<string, Objective[]>();
-        const privateObjectivesArray: Objective[] = [];
-        const expectedPrivateObjective = stubObjectiveCreator['createObjective']('1', privateObjectiveCount);
-        privateObjectivesArray.push(expectedPrivateObjective);
-        privateObjectives.set('key1', privateObjectivesArray);
-        const testTransitionObjectives = onlineObjectiveConverter.convertObjectives(publicObjective, privateObjectives);
-
-        expect(testTransitionObjectives).to.equal(expectedTransitionObjectives);
+        const resultTransitionObjectives = onlineObjectiveConverter.convertObjectives(publicObjectives, privateObjectives);
+        expect(resultTransitionObjectives).to.be.deep.equal(expectedTransitionObjectives);
     });
 });
