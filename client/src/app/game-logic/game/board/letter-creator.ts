381f3b8f6b8ecec0c728fc5f0c9c9ac48460897d
Julie-anne <jaeggi.ja@gmail.com>
2021-12-02 12:29:12 -0500

    aq(letter-creator): missed lint

--- client/src/app/game-logic/game/board/letter-creator.ts
+++ client/src/app/game-logic/game/board/letter-creator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { Letter } from 'src/app/game-logic/game/board/letter.interface';
 
 const GAME_LETTERS = [
@@ -33,9 +34,7 @@ const GAME_LETTERS = [
 const INDEX_RECTIFIER = 'A'.charCodeAt(0);
 export class LetterCreator {
     static readonly gameLetters = GAME_LETTERS;
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     static readonly gameLettersCount = [9, 2, 2, 3, 15, 2, 2, 2, 8, 1, 1, 5, 3, 6, 6, 2, 1, 6, 6, 6, 6, 2, 1, 1, 1, 1, 2];
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     static readonly gameLettersValue = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10, 0];
     static readonly defaultNumberOfLetters = LetterCreator.gameLettersCount.reduce((sum, current) => sum + current, 0);
     indexRectifier = INDEX_RECTIFIER;
